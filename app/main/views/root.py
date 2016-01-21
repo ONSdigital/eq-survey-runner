e21855314cd12a39ba443b9e5f5854640758f6fb
Hamish Taplin <hamish@justpark.com>
2016-01-21 14:05:10 +0000

    added pattern library route and template

--- app/main/views/root.py
+++ app/main/views/root.py
@@ -5,3 +5,8 @@ from .. import main
 @main.route('/', methods=['GET'])
 def root():
     return render_template('index.html')
+
+
+@main.route('/patterns/')
+def hello():
+    return render_template('patterns/index.html')
