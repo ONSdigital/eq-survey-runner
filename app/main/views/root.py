c5f689d13583f3aa483049a189c816e8f0396bf4
Hamish Taplin <hamish@justpark.com>
2016-01-26 13:56:55 +0000

    components now have own route

--- app/main/views/root.py
+++ app/main/views/root.py
@@ -10,15 +10,21 @@ def root():
     return render_template('index.html')
 
 @main.route('/patterns/')
-def patterns():
+@main.route('/patterns/<pattern>')
+def patterns(pattern = "index"):
     sections = []
-    for root, dirs, files in os.walk('app/templates/patterns/components/'):
+
+    for root, dirs, files in os.walk('app/templates/patterns/components'):
         for file in files:
             if file.endswith(".html"):
               with open(os.path.join(root, file), 'r') as f:
                 sections.append({
                   "title": file.replace('.html', ''),
-                  "content": render_template_string(f.read())
                 })
 
-    return render_template('patterns/index.html', sections=sections)
+    if pattern == "index":
+      template = 'patterns/components/grid-system.html'
+    else:
+      template = 'patterns/components/' + pattern + '.html'
+
+    return render_template('patterns/index.html', sections=sections, pattern_include=template)
