db03299a01d313eb881284435994f12ec9788435
David Jones <tafftoo@gmail.com>
2016-02-08 14:20:36 +0000

    Add watchtower logging for cloudwatch logs

--- application.py
+++ application.py
@@ -2,8 +2,10 @@
 
 import os
 from app import create_app
-
 from flask.ext.script import Manager, Server
+import watchtower
+import logging
+
 
 application = create_app(
     os.getenv('EQ_ENVIRONMENT') or 'development'
@@ -12,6 +14,11 @@ application.debug = True
 manager = Manager(application)
 port = int(os.environ.get('PORT', 5000))
 manager.add_command("runserver", Server(host='0.0.0.0', port=port))
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+logger.addHandler(watchtower.CloudWatchLogHandler())
+
 
 if __name__ == '__main__':
     manager.run()
