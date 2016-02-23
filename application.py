22948ec15063fdfb5a20011b94f510a00a5b32f9
David Jones <tafftoo@gmail.com>
2016-02-23 13:27:12 +0000

    Set log format

--- application.py
+++ application.py
@@ -16,7 +16,7 @@ port = int(os.environ.get('PORT', 5000))
 manager.add_command("runserver", Server(host='0.0.0.0', port=port))
 
 cloud_watch_handler = watchtower.CloudWatchLogHandler()
-
+FORMAT = "[%(asctime)s] %(levelname)s [%(name)s.%(funcName)s:%(lineno)d] %(message)s"
 levels = {
     'CRITICAL': logging.CRITICAL,
     'ERROR': logging.ERROR,
@@ -24,7 +24,7 @@ levels = {
     'INFO': logging.INFO,
     'DEBUG': logging.DEBUG
 }
-logging.basicConfig(level=levels[os.getenv('EQ_LOG_LEVEL') or 'WARNING'])
+logging.basicConfig(level=levels[os.getenv('EQ_LOG_LEVEL') or 'WARNING'], format=FORMAT)
 application.logger.addHandler(cloud_watch_handler)
 logging.getLogger().addHandler(cloud_watch_handler)
 logging.getLogger(__name__).addHandler(cloud_watch_handler)
