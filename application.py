13e89979883256c40463954fd419001844ba4fc5
Hamish Taplin <hello@hamishtaplin.com>
2016-02-26 08:59:48 +0000

    made patterns nav sticky

--- application.py
+++ application.py
@@ -15,21 +15,21 @@ manager = Manager(application)
 port = int(os.environ.get('PORT', 5000))
 manager.add_command("runserver", Server(host='0.0.0.0', port=port))
 
-log_group = os.getenv('EQ_SR_LOG_GROUP')
-cloud_watch_handler = watchtower.CloudWatchLogHandler(log_group=log_group)
-FORMAT = "[%(asctime)s] %(levelname)s [%(name)s.%(funcName)s:%(lineno)d] %(message)s"
-levels = {
-    'CRITICAL': logging.CRITICAL,
-    'ERROR': logging.ERROR,
-    'WARNING': logging.WARNING,
-    'INFO': logging.INFO,
-    'DEBUG': logging.DEBUG
-}
-logging.basicConfig(level=levels[os.getenv('EQ_LOG_LEVEL') or 'WARNING'], format=FORMAT)
-application.logger.addHandler(cloud_watch_handler)
-logging.getLogger().addHandler(cloud_watch_handler)
-logging.getLogger(__name__).addHandler(cloud_watch_handler)
-logging.getLogger('werkzeug').addHandler(cloud_watch_handler)
+# log_group = os.getenv('EQ_SR_LOG_GROUP')
+# cloud_watch_handler = watchtower.CloudWatchLogHandler(log_group=log_group)
+# FORMAT = "[%(asctime)s] %(levelname)s [%(name)s.%(funcName)s:%(lineno)d] %(message)s"
+# levels = {
+#     'CRITICAL': logging.CRITICAL,
+#     'ERROR': logging.ERROR,
+#     'WARNING': logging.WARNING,
+#     'INFO': logging.INFO,
+#     'DEBUG': logging.DEBUG
+# }
+# logging.basicConfig(level=levels[os.getenv('EQ_LOG_LEVEL') or 'WARNING'], format=FORMAT)
+# application.logger.addHandler(cloud_watch_handler)
+# logging.getLogger().addHandler(cloud_watch_handler)
+# logging.getLogger(__name__).addHandler(cloud_watch_handler)
+# logging.getLogger('werkzeug').addHandler(cloud_watch_handler)
 
 
 if __name__ == '__main__':
