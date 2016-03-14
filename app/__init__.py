2dd95443447c1bf6a222befef5514be00a8731a9
David Jones <tafftoo@gmail.com>
2016-03-14 15:56:27 +0000

    Moved jinja filters into app

--- app/__init__.py
+++ app/__init__.py
@@ -14,7 +14,6 @@ import newrelic.agent
 import watchtower
 import logging
 
-
 newrelic_config = settings.EQ_NEW_RELIC_CONFIG_FILE
 if os.path.isfile(newrelic_config):
     newrelic.agent.initialize(newrelic_config)
@@ -80,8 +79,8 @@ def create_app(config_name):
     from .patternlib import patternlib_blueprint
     application.register_blueprint(patternlib_blueprint)
 
-    import jinja_filters
-    application.register_blueprint(jinja_filters.blueprint)
+    from app.jinja_filters import blueprint as filter_blueprint
+    application.register_blueprint(filter_blueprint)
 
     # set up some sane logging, as opposed to what flask does by default
     FORMAT = "[%(asctime)s] %(levelname)s [%(name)s.%(funcName)s:%(lineno)d] %(message)s"
