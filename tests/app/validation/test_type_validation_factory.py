fd2fa502d28c15c315ccccd62cd09bc941de8631
David Jones <tafftoo@gmail.com>
2016-03-17 11:25:35 +0000

    Add date type check

--- tests/app/validation/test_type_validation_factory.py
+++ tests/app/validation/test_type_validation_factory.py
@@ -1,15 +1,20 @@
 import unittest
 from app.validation.type_validator_factory import TypeValidatorFactory, TypeValidatorFactoryException
 from app.validation.integer_type_check import IntegerTypeCheck
+from app.validation.date_type_check import DateTypeCheck
 
 
 class TypeValidatorFactoryTest(unittest.TestCase):
     def test_get_validators_by_type(self):
 
-        # Currently only Integer type is supported
         validators = TypeValidatorFactory.get_validators_by_type('integer')
 
         self.assertEquals(len(validators), 1)
         self.assertTrue(isinstance(validators[0], IntegerTypeCheck))
+
+        validators = TypeValidatorFactory.get_validators_by_type('daterange')
+
+        self.assertEquals(len(validators), 1)
+        self.assertTrue(isinstance(validators[0], DateTypeCheck))
 
         self.assertRaises(TypeValidatorFactoryException, TypeValidatorFactory.get_validators_by_type, "unknown_type")
