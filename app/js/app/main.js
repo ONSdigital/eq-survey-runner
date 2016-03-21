bee31509b3cc72687bdb965b75205ce2e318436a
Hamish Taplin <hello@hamishtaplin.com>
2016-03-21 14:45:03 +0000

    moved js code into modules

--- app/js/app/main.js
+++ app/js/app/main.js
@@ -1,15 +1,7 @@
 import 'babel-polyfill'
+import './modules/details'
+import charLimit from './modules/charlimit'
 
-$('.js-details').each((index, el) => {
-  const $el = $(el)
-  const $trigger = $el.find('.js-details-trigger')
-  $trigger.on('click', (e) => {
-    e.preventDefault()
-    $trigger.attr('aria-expanded', 'true')
-    $el.addClass('is-expanded')
-      .find('.js-details-main')
-      .focus()
-      .attr('aria-hidden', 'false')
-    return false
-  })
+$(() => {
+  charLimit()
 })
---
+++ app/js/app/modules/details.js
@@ -0,0 +1,15 @@
+// TODO: refactor this
+
+$('.js-details').each((index, el) => {
+  const $el = $(el)
+  const $trigger = $el.find('.js-details-trigger')
+  $trigger.on('click', (e) => {
+    e.preventDefault()
+    $trigger.attr('aria-expanded', 'true')
+    $el.addClass('is-expanded')
+      .find('.js-details-main')
+      .focus()
+      .attr('aria-hidden', 'false')
+    return false
+  })
+})
