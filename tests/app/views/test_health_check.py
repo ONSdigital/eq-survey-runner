472f23b2f557f72ccde414faf06f7b841d9a88ce
Warren Bailey <warren@warrenbailey.net>
2016-03-24 11:51:58 +0000

    Health check test will now work as the log submitter can be used

--- tests/app/views/test_health_check.py
+++ tests/app/views/test_health_check.py
@@ -1,8 +1,7 @@
 def test_healthcheck(survey_runner):
     response = survey_runner.test_client().get('/healthcheck')
-    # health check will fail as rabbit mq won't be running
-    assert response.status_code == 500
-    assert "failure" in response.get_data(as_text=True)
+    assert response.status_code == 200
+    assert "success" in response.get_data(as_text=True)
 
 
 def test_errors_404(survey_runner):
