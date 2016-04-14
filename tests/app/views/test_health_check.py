9309769f67c20d479463b05ddcf33a221373800f
David Jones <tafftoo@gmail.com>
2016-04-14 11:02:02 +0100

    Prevent over-riding of error page title

--- tests/app/views/test_health_check.py
+++ tests/app/views/test_health_check.py
@@ -7,4 +7,4 @@ def test_healthcheck(survey_runner):
 def test_errors_404(survey_runner):
     response = survey_runner.test_client().get('/hfjdskahfjdkashfsa')
     assert response.status_code == 404
-    assert "An error has occurred" in response.get_data(as_text=True)
+    assert '<title>Error</title' in response.get_data(True)
