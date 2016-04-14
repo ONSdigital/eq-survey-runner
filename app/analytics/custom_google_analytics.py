2e5ea0c27998b8d66d6ec8be83adc1c52742110d
David Jones <tafftoo@gmail.com>
2016-04-14 14:08:26 +0100

    Update google analytics to use new async style and anonymize ips

--- app/analytics/custom_google_analytics.py
+++ app/analytics/custom_google_analytics.py
@@ -9,18 +9,19 @@ class CustomGoogleAnalytics(GoogleAnalytics):
 
     @property
     def template(self):
-            return """<!-- Google Analytics -->
-<script>
-window.ga=window.ga||function(){(ga.q=ga.q||[]).push(arguments)};ga.l=+new Date;
-ga('create', '{account}', 'auto');
+            return """<script>
+(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+ga('create', '%s', 'auto');
 ga('set', 'anonymizeIp', true);
 ga('send', 'pageview');
-</script>
-<script async src='https://www.google-analytics.com/analytics.js'></script>
-<!-- End Google Analytics -->"""
+</script>"""
 
     @property
     def source(self):
         if self.account is None:
             return None
-        return self.template.format(account=self.account)
+        return self.template % self.account
