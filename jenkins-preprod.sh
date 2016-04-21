8691c6adb7589262fff7f395caba2eef7de272c8
Warren Bailey <warren@warrenbailey.net>
2016-04-21 17:04:03 +0100

    Fixing password filename

--- jenkins-preprod.sh
+++ jenkins-preprod.sh
@@ -14,7 +14,7 @@ cp secure-data/sdc-user-authentication-encryption-sr-public-key.pem key_mat
 
 EQ_USER_AUTHENTICATION_RRM_PUBLIC_KEY="./key_mat/sdc-user-authentication-signing-rrm-public-key.pem"
 EQ_USER_AUTHENTICATION_SR_PRIVATE_KEY="./key_mat/sdc-user-authentication-encryption-sr-private-key.pem"
-EQ_USER_AUTHENTICATION_SR_PRIVATE_KEY_PASSWORD=`cat secure-data/sdc-user-authentication-encryption-sr-private-key.txt`
+EQ_USER_AUTHENTICATION_SR_PRIVATE_KEY_PASSWORD=`cat secure-data/sdc-user-authentication-encryption-sr-private-key-password.txt`
 
 # needed for DEV mode
 EQ_USER_AUTHENTICATION_RRM_PRIVATE_KEY="./key_mat/sdc-user-authentication-signing-rrm-private-key.pem"
--- jenkins-prod.sh
+++ jenkins-prod.sh
@@ -14,7 +14,7 @@ cp secure-data/sdc-submission-signing-sr-private-key.pem key_mat
 
 EQ_USER_AUTHENTICATION_RRM_PUBLIC_KEY="./key_mat/sdc-user-authentication-signing-rrm-public-key.pem"
 EQ_USER_AUTHENTICATION_SR_PRIVATE_KEY="./key_mat/sdc-user-authentication-encryption-sr-private-key.pem"
-EQ_USER_AUTHENTICATION_SR_PRIVATE_KEY_PASSWORD=`cat secure-data/sdc-user-authentication-encryption-sr-private-key.txt`
+EQ_USER_AUTHENTICATION_SR_PRIVATE_KEY_PASSWORD=`cat secure-data/sdc-user-authentication-encryption-sr-private-key-password.txt`
 
 EQ_SUBMISSION_SDX_PUBLIC_KEY="./key_mat/sdc-submission-encryption-sdx-public-key.pem"
 EQ_SUBMISSION_SR_PRIVATE_SIGNING_KEY="./key_mat/sdc-submission-signing-sr-private-key.pem"
