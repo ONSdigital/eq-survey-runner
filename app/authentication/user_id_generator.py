a894fd4ade7f75cc52716e8d07210b5b98618d04
Warren Bailey <warren@warrenbailey.net>
2016-05-13 15:31:58 +0100

    Code review comments fixes

--- app/authentication/user_id_generator.py
+++ app/authentication/user_id_generator.py
@@ -1,4 +1,5 @@
 from app import settings
+from app.authentication.invalid_token_exception import InvalidTokenException
 from app.metadata.metadata_store import MetaDataConstants
 from app.utilities.strings import to_str
 import hashlib
@@ -16,15 +17,19 @@ class UserIDGenerator(object):
         collection_exercise_sid = token.get(MetaDataConstants.COLLECTION_EXERCISE_SID)
         eq_id = token.get(MetaDataConstants.EQ_ID)
 
-        logger.debug("Using values %s, %s and %s with a salt", ru_ref, collection_exercise_sid, eq_id)
-        salt = settings.EQ_SERVER_SIDE_STORAGE_SALT
+        if ru_ref and collection_exercise_sid and eq_id:
+            logger.debug("Using values %s, %s and %s with a salt", ru_ref, collection_exercise_sid, eq_id)
+            salt = settings.EQ_SERVER_SIDE_STORAGE_USER_ID_SALT
 
-        sha256 = hashlib.sha256()
-        sha256.update(to_str(ru_ref).encode('utf-8'))
-        sha256.update(to_str(collection_exercise_sid).encode('utf-8'))
-        sha256.update(to_str(eq_id).encode('utf-8'))
-        sha256.update(to_str(salt).encode('utf-8'))
+            sha256 = hashlib.sha256()
+            sha256.update(to_str(ru_ref).encode('utf-8'))
+            sha256.update(to_str(collection_exercise_sid).encode('utf-8'))
+            sha256.update(to_str(eq_id).encode('utf-8'))
+            sha256.update(to_str(salt).encode('utf-8'))
 
-        user_id = sha256.hexdigest()
-        logger.debug("User ID is %s", user_id)
-        return user_id
+            user_id = sha256.hexdigest()
+            logger.debug("User ID is %s", user_id)
+            return user_id
+        else:
+            logger.error("Missing values for ru_ref, collection_exercise_sid or eq_id in token %s", token)
+            raise InvalidTokenException("Missing values in JWT token")
