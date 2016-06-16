b3baabd0cb9837d6ddf1c632c0f7a928555dfb1f
Hamish Taplin <hello@hamishtaplin.com>
2016-06-16 11:53:56 +0100

    added guidance event tracking

--- app/js/app/modules/analytics.js
+++ app/js/app/modules/analytics.js
@@ -3,7 +3,9 @@ import domready from './domready'
 
 export default function initAnalytics() {
   const errors = document.querySelectorAll('[data-error=true]')
-  forEach(errors, (error) => {
+  const guidances = document.querySelectorAll('[data-guidance]')
+
+  forEach(errors, error => {
     const errorMsg = error.getAttribute('data-error-msg')
     const errorValue = error.getAttribute('data-error-value')
     const elementId = error.getAttribute('data-error-id')
@@ -15,6 +17,26 @@ export default function initAnalytics() {
     }
     console.log(errorData)
     ga('send', errorData)
+  })
+
+  forEach(guidances, guidance => {
+    const trigger = guidance.querySelector('[data-guidance-trigger]')
+    const questionLabel = guidance.getAttribute('data-guidance-label')
+    const questionId = guidance.getAttribute('data-guidance')
+
+    const onClick = e => {
+      trigger.removeEventListener('click', onClick)
+      const triggerData = {
+        hitType: 'event',
+        eventCategory: 'Guidance',
+        eventAction: questionLabel,
+        eventLabel: questionId
+      }
+      console.log(triggerData)
+      ga('send', triggerData)
+    }
+
+    trigger.addEventListener('click', onClick)
   })
 }
 
