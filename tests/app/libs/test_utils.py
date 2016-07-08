b6ed53f489b6313f15e758a69688b6d0c80fc842
David Jones <tafftoo@gmail.com>
2016-07-08 10:29:05 +0100

    Remove unused funcs, and add test for remaining

---
+++ tests/app/libs/test_utils.py
@@ -0,0 +1,16 @@
+import unittest
+from app.libs.utils import ObjectFromDict
+
+class ObjectFromDictTest(unittest.TestCase):
+    def test_simple_object(self):
+        simple_dict = {
+            'property_one': 'string',
+            'property_two': 2,
+            'property_three': []
+        }
+
+        obj = ObjectFromDict(simple_dict)
+
+        self.assertEquals(obj.property_one, 'string')
+        self.assertEquals(obj.property_two, 2)
+        self.assertEquals(len(obj.property_three), 0)
