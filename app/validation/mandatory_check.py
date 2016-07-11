43da1a5aec939bc99d39a139ea93110964d5c70e
David Jones <tafftoo@gmail.com>
2016-07-11 11:20:25 +0100

    Refactored mandatory check

--- app/validation/mandatory_check.py
+++ app/validation/mandatory_check.py
@@ -11,26 +11,29 @@ class MandatoryCheck(AbstractValidator):
     """
 
     def validate(self, user_answer):
-
         validation_result = ValidationResult()
+        validation_result.is_valid = False
 
         if isinstance(user_answer, list):
-            for answer in user_answer:
-                if answer and not str(answer).isspace():
-                    validation_result.is_valid = True
-                    return validation_result
-                else:
-                    validation_result.is_valid = False
-                    # We do not return at this point as there may still be
-                    # a valid response in the list
-
-            # No valid values in list
-            validation_result.errors.append(AbstractValidator.MANDATORY)
+            self._validate_list(user_answer, validation_result)
         else:
-            if user_answer and not str(user_answer).isspace():
-                validation_result.is_valid = True
-            else:
-                validation_result.is_valid = False
-                validation_result.errors.append(AbstractValidator.MANDATORY)
+            self._validate_single(user_answer, validation_result)
+
+        # We only want ONE error message
+        if not validation_result.is_valid:
+            validation_result.errors.append(AbstractValidator.MANDATORY)
 
         return validation_result
+
+    def _validate_list(self, user_answers, validation_result):
+        for answer in user_answers:
+            self._validate_single(answer, validation_result)
+            if validation_result.is_valid:
+                # We've found a valid entry in the list, bail out
+                return
+
+    def _validate_single(self, user_answer, validation_result):
+        if user_answer and not str(user_answer).isspace():
+            validation_result.is_valid = True
+        else:
+            validation_result.is_valid = False
