2282b1029e4471de3e20a83ae7887802fa4fe878
Warren Bailey <warren@warrenbailey.net>
2016-07-13 11:56:25 +0100

    Fixing the submission failed exception and adding a catch all block so we don't display python exceptions in the web browser

--- app/main/views/questionnaire.py
+++ app/main/views/questionnaire.py
@@ -5,9 +5,10 @@ from flask_login import login_required, current_user
 from flask import redirect
 from app.questionnaire.create_questionnaire_manager import create_questionnaire_manager
 from app.submitter.converter import SubmitterConstants
+from app.submitter.submission_failed import SubmissionFailedException
 from .. import main_blueprint
 from app.schema.questionnaire import QuestionnaireException
-from app.main.errors import page_not_found
+from app.main.errors import page_not_found, internal_server_error, service_unavailable
 from flask.ext.themes2 import render_theme_template
 from app.metadata.metadata_store import MetaDataStore
 
@@ -38,33 +39,39 @@ def survey(eq_id, collection_id, location):
 
         # Process the POST request
         if request.method == 'POST':
-            logger.debug("POST request question - current location %s", location)
-            questionnaire_manager.process_incoming_answers(request.form)
-            next_location = questionnaire_manager.get_current_location()
+            return do_post(collection_id, eq_id, location, questionnaire_manager)
+        else:
+            return do_get(questionnaire_manager)
 
-            current_user.save()
-            metadata = MetaDataStore.get_instance(current_user)
+    except QuestionnaireException as e:
+        return page_not_found(e)
+    except SubmissionFailedException as e:
+        # Rabbit MQ connection issue
+        return service_unavailable(e)
+    except Exception as e:
+        return internal_server_error(e)
 
-            logger.info("Redirecting user to next location %s with tx_id=%s", next_location, metadata.tx_id)
-            return redirect('/questionnaire/' + eq_id + '/' + collection_id + '/' + next_location)
 
-        context = questionnaire_manager.get_rendering_context()
-        template = questionnaire_manager.get_rendering_template()
-        try:
-            theme = context['meta']['survey']['theme']
-            logger.info("Theme selected: {} ".format(theme))
-        except KeyError:
-            logger.info("No theme set ")
-            theme = None
-        return render_theme_template(theme,
-                                     template,
-                                     meta=context['meta'],
-                                     content=context['content'],
-                                     navigation=context['navigation']
-                                     )
+def do_get(questionnaire_manager):
+    context = questionnaire_manager.get_rendering_context()
+    template = questionnaire_manager.get_rendering_template()
+    try:
+        theme = context['meta']['survey']['theme']
+        logger.info("Theme selected: {} ".format(theme))
+    except KeyError:
+        logger.info("No theme set ")
+        theme = None
+    return render_theme_template(theme, template, meta=context['meta'], content=context['content'], navigation=context['navigation'])
 
-    except QuestionnaireException:
-        return page_not_found(404)
+
+def do_post(collection_id, eq_id, location, questionnaire_manager):
+    logger.debug("POST request question - current location %s", location)
+    questionnaire_manager.process_incoming_answers(request.form)
+    next_location = questionnaire_manager.get_current_location()
+    current_user.save()
+    metadata = MetaDataStore.get_instance(current_user)
+    logger.info("Redirecting user to next location %s with tx_id=%s", next_location, metadata.tx_id)
+    return redirect('/questionnaire/' + eq_id + '/' + collection_id + '/' + next_location)
 
 
 def delete_user_data():
