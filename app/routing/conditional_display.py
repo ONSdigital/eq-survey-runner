586961715f629d1f1c876d46557c13a397a2835a
Warren Bailey <warren@warrenbailey.net>
2016-08-10 10:53:11 +0100

    Do not display skipped questions on summary page

--- app/routing/conditional_display.py
+++ app/routing/conditional_display.py
@@ -13,12 +13,12 @@ class ConditionalDisplay(object):
         if item.skip_condition:
             when = item.skip_condition.when
             if questionnaire_manager:
-                logger.error("ConditionalDisplay - when id is %s", when.id)
-                logger.error("ConditionalDisplay - when value is %s", when.value)
-                logger.error("ConditionalDisplay - when condition is %s", when.condition)
+                logger.debug("ConditionalDisplay - when id is %s", when.id)
+                logger.debug("ConditionalDisplay - when value is %s", when.value)
+                logger.debug("ConditionalDisplay - when condition is %s", when.condition)
                 answer = questionnaire_manager.find_answer(when.id)
-                logger.error("ConditionalDisplay - answer is %s", answer)
+                logger.debug("ConditionalDisplay - answer is %s", answer)
                 if (when.condition == Condition.NOT_EQUALS and when.value != answer) or (when.condition == Condition.EQUALS and when.value == answer):
                         skipped = True
-                logger.error("ConditionalDisplay - skipped is %s", skipped)
+                logger.debug("ConditionalDisplay - skipped is %s", skipped)
         return skipped
