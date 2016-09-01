ba76f251c5f7fbf8c6aff5c2ab78dd6d88e4a7ad
Benn Pearson <bennpearson@gmail.com>
2016-09-01 18:01:29 +0100

    added focus to error message link to improve screen reader ux

---
+++ app/assets/js/app/modules/inpagelink.js
@@ -0,0 +1,38 @@
+import forEach from 'lodash/forEach'
+import domready from './domready'
+
+export const classDetails = 'js-inpagelink'
+export const classTrigger = 'js-inpagelink-trigger'
+
+export const attrInputId = 'data-inputid'
+
+export default function() {
+  return inPageLink()
+}
+
+export function inPageLink() {
+  const nodeList = document.getElementsByClassName(classDetails)
+  forEach(nodeList, applyInPageLink)
+  return nodeList
+}
+
+export function applyInPageLink(elDetails) {
+  const elTrigger = elDetails.getElementsByClassName(classTrigger)[0]
+  const elId = elDetails.getAttribute(attrInputId)
+  elTrigger.addEventListener('click', (e) => {
+    e.preventDefault()
+    focusOnInput(elId)
+  })
+
+  return {
+    elId: elId,
+    elTrigger: elTrigger
+  }
+}
+
+function focusOnInput(elId) {
+  const elIdInput = document.getElementById(elId).getElementsByClassName('input')[0]
+  elIdInput.focus()
+}
+
+domready(inPageLink)
