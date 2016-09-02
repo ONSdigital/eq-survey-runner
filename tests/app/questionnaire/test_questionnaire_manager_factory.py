b21c5f8c91c4550e3ff06c7cccfa553f5d5ab2ca
Lewis Babbage <Lewis@Lewiss-MacBook-Pro.local>
2016-09-02 10:54:40 +0100

    Fix migration problem not having the revision attribute and add tests

--- tests/app/questionnaire/test_questionnaire_manager_factory.py
+++ tests/app/questionnaire/test_questionnaire_manager_factory.py
@@ -1,3 +1,5 @@
+import logging
+
 from app.questionnaire.questionnaire_manager_factory import QuestionnaireManagerFactory
 from app.questionnaire_state.node import Node
 from app.questionnaire.state import State
@@ -31,12 +33,26 @@ class TestQuestionnaireManagerFactory(SurveyRunnerTestCase):
 
     def test_get_instance_git_revision_change(self):
         with self.application.test_request_context():
-            StateManager.has_state = MagicMock(return_value="True")
-            node = Node("1", None)
-            state = State(self.questionnaire, current_node=node, first_node=node, tail_node=node, valid_locations=[], archive={}, submitted_at=None)
+            state = self.mock_state()
             state.revision = "1"
             StateManager.get_state = MagicMock(return_value=state)
             StateRecovery.recover_from_post_data = MagicMock()
             questionnaire_manager = QuestionnaireManagerFactory.get_instance()
             StateRecovery.recover_from_post_data.assert_called_with(questionnaire_manager)
 
+    def test_missing_revision_attribute(self):
+        with self.application.test_request_context():
+            state = self.mock_state()
+            del state.revision
+            StateManager.get_state = MagicMock(return_value=state)
+            logger_name = 'app.questionnaire.questionnaire_manager_factory'
+            logging.getLogger(logger_name).error = MagicMock()
+            QuestionnaireManagerFactory.get_instance()
+            logging.getLogger(logger_name).error.assert_called_with('Data not recoverable, old version of state without revision attribute')
+
+    def mock_state(self):
+        StateManager.has_state = MagicMock(return_value="True")
+        node = Node("1", None)
+        state = State(self.questionnaire, current_node=node, first_node=node, tail_node=node, valid_locations=[],
+                    archive={}, submitted_at=None)
+        return state
