c8555012dcf18080584104d91e1ea10357897947
iwootten <i.wootten@gmail.com>
2016-09-05 18:26:12 +0100

    move factory creation to group level

--- app/templating/summary/block.py
+++ app/templating/summary/block.py
@@ -1,4 +1,7 @@
-from app.templating.summary.summary_item import SummaryItem
+from app.templating.summary.items.date_range_summary import DateRangeSummaryItem
+from app.templating.summary.items.general_summary import GeneralSummaryItem
+
+from app.utilities.factory import Factory
 
 
 class Block(object):
@@ -11,8 +14,18 @@ class Block(object):
     def build_summary_items(self, state, schema):
         items = []
 
+        question_factory = Factory()
+        question_factory.register_all({
+            "DATERANGE": DateRangeSummaryItem,
+            "GENERAL": GeneralSummaryItem,
+        })
+
         for question_state in state.questions:
             if question_state.display_on_summary:
-                items.append(SummaryItem.create_item(schema.questionnaire.get_item_by_id(question_state.id), question_state))
+                schema_item = schema.questionnaire.get_item_by_id(question_state.id)
+
+                item = question_factory.create(schema_item.type.upper(), schema_item, question_state)
+
+                items.append(item)
 
         return items
--- app/templating/summary/summary_item.py
+++ app/templating/summary/summary_item.py
@@ -1,6 +1,3 @@
-from app.utilities.factory import Factory
-
-
 class SummaryItem(object):
     def __init__(self, schema, state):
         self.schema = schema
@@ -9,15 +6,3 @@ class SummaryItem(object):
         self.link = self.schema.container.container.id + '#' + self.schema.id
         if len(self.schema.answers) > 0:
             self.type = self.schema.answers[0].type.lower()
-
-    @staticmethod
-    def create_item(schema, state):
-        from app.templating.summary.items.date_range_summary import DateRangeSummaryItem
-        from app.templating.summary.items.general_summary import GeneralSummaryItem
-
-        factory = Factory()
-        factory.register_all({
-            "DATERANGE": DateRangeSummaryItem,
-            "GENERAL": GeneralSummaryItem,
-        })
-        return factory.create(schema.type.upper(), schema, state)
