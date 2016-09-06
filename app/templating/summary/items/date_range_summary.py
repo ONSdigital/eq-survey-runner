5acaf4441e8dd1223f883f06d175f9ce4c1ab3e9
iwootten <i.wootten@gmail.com>
2016-09-06 10:43:07 +0100

    Correct indentation

--- app/templating/summary/items/date_range_summary.py
+++ app/templating/summary/items/date_range_summary.py
@@ -5,9 +5,9 @@ class DateRangeSummaryItem(SummaryItem):
     def __init__(self, schema, state):
         super().__init__(schema, state)
         self.answer = [{
-          'label': schema.answers[0].label,
-          'value': state.answers[0].value,
+            'label': schema.answers[0].label,
+            'value': state.answers[0].value,
         }, {
-          'label': schema.answers[1].label,
-          'value': state.answers[1].value,
+            'label': schema.answers[1].label,
+            'value': state.answers[1].value,
         }]
