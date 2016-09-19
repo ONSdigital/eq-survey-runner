f448d65974ceb833e1d9f98e01af59ff998e5e85
Hamish Taplin <hello@hamishtaplin.com>
2016-09-19 15:01:13 +0100

    appease the lint gods

--- app/frontend_messages.py
+++ app/frontend_messages.py
@@ -1,4 +1,4 @@
 messages = {
   'MULTIPLE_SURVEYS': "Unfortunately you can only complete one survey at a time.",
-  'MULTIPLE_SURVEYS_1': "Close this window to continue with your current survey."
+  'MULTIPLE_SURVEYS_1': "Close this window to continue with your current survey.",
 }
