0dd4c11c5c286b0fdf92022ca481d2442fb4e04d
Samiwel Thomas <samiwel.thomas@gmail.com>
2016-10-06 15:08:33 +0100

    Fix unit test failures.

--- app/schema/widget.py
+++ app/schema/widget.py
@@ -17,10 +17,10 @@ class Widget(object):
         return user_input
 
     def get_other_input(self, post_vars):
-        list_of_values = post_vars.getlist(self.name)
-        if not list_of_values:
+        if not hasattr(post_vars, 'getlist'):
             return None
+        list_of_values = post_vars.getlist(self.name)
         logger.debug('There are {} entries in the list: {}'.format(len(list_of_values), list_of_values))
-        user_input = list_of_values[-1:][0]
+        user_input = list_of_values[-1:][0] if len(list_of_values) > 0 else None
         logger.debug('Getting user input for "{}", value is "{}"'.format(self.name, user_input))
         return user_input
