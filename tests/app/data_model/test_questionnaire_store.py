5bdadf131c8791a0f84b551a84b784a17ec2c927
iwootten <i.wootten@gmail.com>
2016-10-10 13:38:12 +0100

    Add some tests to cover questionnaire store exceptions

---
+++ tests/app/data_model/test_questionnaire_store.py
@@ -0,0 +1,17 @@
+import unittest
+from app.data_model.questionnaire_store import QuestionnaireStore, get_questionnaire_store
+
+
+class TestQuestionnaireStore(unittest.TestCase):
+
+    def test_anonymous_user_throws_error(self):
+        with self.assertRaises(ValueError) as ite:
+            QuestionnaireStore(None, None)
+            self.assertIn("No user_id or user_ik found in session", ite.exception.value)
+
+    def test_empty_metadata_throws_error(self):
+        with self.assertRaises(RuntimeError) as ite:
+            qStore = QuestionnaireStore("1", "2")
+            qStore.decode_metadata()
+            self.assertIn("No metadata for user 1", ite.exception.value)
+
