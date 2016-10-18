eda099d3320bd5e0ea0df6a655792df6ec0f4cac
iwootten <i.wootten@gmail.com>
2016-10-18 15:41:37 +0100

    Remove loops to make tests obvious

--- tests/app/utilities/test_date_utils.py
+++ tests/app/utilities/test_date_utils.py
@@ -7,29 +7,21 @@ from app.utilities.date_utils import to_date
 class TestDateUtils(unittest.TestCase):
 
     def test_parse_valid_date(self):
-        date_strings = ("2016-04-01", "2016-09-09")
-
-        for date_str in date_strings:
-            parsed_date = to_date(date_str)
-
-            self.assertTrue(isinstance(parsed_date, datetime.date))
+        self.assertTrue(isinstance(to_date("2016-04-01"), datetime.date))
+        self.assertTrue(isinstance(to_date("2016-09-09"), datetime.date))
 
     def test_parse_invalid_date(self):
-        date_strings = ("2016-Mar-03", "a-random-string")
-
-        for date_str in date_strings:
-            parsed_date = to_date(date_str)
-
-            self.assertEquals(parsed_date, None)
+        self.assertEquals(to_date("2016-Mar-03"), None)
+        self.assertEquals(to_date("a-random-string"), None)
 
     def test_custom_date_string(self):
         custom_format = "%y-%b-%d"
-        date_strings = ("16-Mar-03", "16-Aug-13")
 
-        for date_str in date_strings:
-            parsed_date = to_date(date_str, custom_format)
+        parsed_date_1 = to_date("16-Mar-03", custom_format)
+        parsed_date_2 = to_date("16-Aug-13", custom_format)
 
-            self.assertTrue(isinstance(parsed_date, datetime.date))
+        self.assertTrue(isinstance(parsed_date_1, datetime.date))
+        self.assertTrue(isinstance(parsed_date_2, datetime.date))
 
     def test_parse_empty_date(self):
         date_str = None
