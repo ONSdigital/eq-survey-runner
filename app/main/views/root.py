05f262cc2297f0e669c3bd859cd3cd3a0a49ea7b
Ben Jefferies <benjefferies@echosoft.uk>
2016-10-21 15:27:16 +0100

    Split out end points
    * Split questionnaire submit and get requests
    * Pull out thank-you page
    * Pull out previous
    * Remove first end point
      * first end point only ever appeared to be used in integration test
    * Making use of flasks error handling framework to take care of
      exception handling
    * Using flask.before_request to check questionnaire state
    * Delete unused methods
      * The delete_user_data function was only used by the questionnaire end
        point and not in the questionnaire_store therefore refactoring to the
        relevant place.
      * get_current_state methods
      * _previous
      * submitted

--- app/main/views/root.py
+++ app/main/views/root.py
@@ -1,44 +1,49 @@
 import logging
 
 from app.authentication.authenticator import Authenticator
-from app.authentication.invalid_token_exception import InvalidTokenException
-from app.authentication.no_token_exception import NoTokenException
 from app.data_model.questionnaire_store import get_metadata
 from app.frontend_messages import get_messages
-from app.main import errors
 from app.questionnaire.questionnaire_manager_factory import QuestionnaireManagerFactory
 
-from flask import abort
 from flask import redirect
 from flask import request
 from flask import session
+from flask import Blueprint
 
 from flask.ext.themes2 import render_theme_template
 
 from flask_login import current_user
 
-
-from .. import main_blueprint
+from werkzeug.exceptions import NotFound
 
 logger = logging.getLogger(__name__)
 
 
-@main_blueprint.route('/', methods=['GET'])
+root_blueprint = Blueprint('root', __name__)
+
+
+@root_blueprint.after_request
+def add_cache_control(response):
+    response.cache_control.no_cache = True
+    return response
+
+
+@root_blueprint.route('/', methods=['GET'])
 def root():
-    return errors.index()
+    raise NotFound
 
 
-@main_blueprint.route('/information/<message_identifier>', methods=['GET'])
+@root_blueprint.route('/information/<message_identifier>', methods=['GET'])
 def information(message_identifier):
     front_end_message = get_messages(message_identifier)
     if front_end_message:
         logger.debug(front_end_message)
         return render_theme_template('default', 'information.html',
                                      messages=front_end_message)
-    return errors.page_not_found()
+    raise NotFound
 
 
-@main_blueprint.route('/session', methods=['GET'])
+@root_blueprint.route('/session', methods=['GET'])
 def login():
     """
     Initial url processing - expects a token parameter and then will authenticate this token. Once authenticated
@@ -52,34 +57,25 @@ def login():
 
     authenticator = Authenticator()
     logger.debug("Attempting token authentication")
-    try:
-        authenticator.jwt_login(request)
-        logger.debug("Token authenticated - linking to session")
 
-        metadata = get_metadata(current_user)
+    authenticator.jwt_login(request)
+    logger.debug("Token authenticated - linking to session")
 
-        eq_id = metadata["eq_id"]
-        collection_id = metadata["collection_exercise_sid"]
-        form_type = metadata["form_type"]
-        period_id = metadata["period_id"]
+    metadata = get_metadata(current_user)
 
-        logger.debug("Requested questionnaire %s for form type %s", eq_id, form_type)
-        if not eq_id or not form_type:
-            logger.error("Missing EQ id %s or form type %s in JWT", eq_id, form_type)
-            abort(404)
+    eq_id = metadata["eq_id"]
+    collection_id = metadata["collection_exercise_sid"]
+    form_type = metadata["form_type"]
+    period_id = metadata["period_id"]
 
-        questionnaire_manager = QuestionnaireManagerFactory.get_instance()
+    logger.debug("Requested questionnaire %s for form type %s", eq_id, form_type)
+    if not eq_id or not form_type:
+        logger.error("Missing EQ id %s or form type %s in JWT", eq_id, form_type)
+        raise NotFound
 
-        # get the current location of the user
-        current_location = questionnaire_manager.get_current_location()
+    questionnaire_manager = QuestionnaireManagerFactory.get_instance()
 
-        return redirect('/questionnaire/' + eq_id + '/' + form_type + '/' + period_id + '/' + collection_id + '/' + current_location)
+    # get the current location of the user
+    current_location = questionnaire_manager.get_current_location()
 
-    except NoTokenException as e:
-        logger.warning("Unable to authenticate user")
-        return errors.unauthorized(e)
-    except InvalidTokenException as e:
-        logger.warning("Invalid Token provided")
-        return errors.forbidden(e)
-    except (Exception, RuntimeError) as e:
-        return errors.internal_server_error(e)
+    return redirect('/questionnaire/' + eq_id + '/' + form_type + '/' + period_id + '/' + collection_id + '/' + current_location)
---
+++ tests/integration/login/test_login.py
@@ -0,0 +1,36 @@
+from tests.integration.create_token import create_token
+from tests.integration.integration_test_case import IntegrationTestCase
+
+
+class TestLogin(IntegrationTestCase):
+
+    def test_login_with_no_token_should_be_unauthorized(self):
+        # Given
+        token = ""
+
+        # When
+        resp = self.client.get('/session?token=' + token, follow_redirects=False)
+
+        # Then
+        self.assertEquals(resp.status_code, 401)
+
+    def test_login_with_invalid_token_should_be_forbidden(self):
+        # Given
+        token = "123"
+
+        # When
+        resp = self.client.get('/session?token=' + token, follow_redirects=False)
+
+        # Then
+        self.assertEquals(resp.status_code, 403)
+
+    def test_login_with_valid_token_should_redirect_to_survey(self):
+        # Given
+        token = create_token('0205', '1')
+
+        # When
+        resp = self.client.get('/session?token=' + token.decode(), follow_redirects=False)
+
+        # Then
+        self.assertEquals(resp.status_code, 302)
+        self.assertRegex(resp.location, '/questionnaire/1/0205')
