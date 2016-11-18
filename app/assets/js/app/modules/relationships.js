ee29df54de0a692fc51a5ea973453f11aabf53e2
Hamish Taplin <hello@hamishtaplin.com>
2016-11-18 15:16:03 +0000

    more work on templates

---
+++ app/assets/js/app/modules/relationships.js
@@ -0,0 +1,13 @@
+import domready from './domready'
+
+domready(() => {
+  // $('#relationship-question').addClass('js-accordion')
+  // $('.question__answer').each((index, element) => {
+  //   const $editBtn = $('<button>').html('Edit').addClass('btn btn--link question__edit js-accordion-trigger')
+  //   $(element)
+  //     .addClass('js-accordion-item')
+  //     .append($editBtn)
+  //     .find('.field__item')
+  //     .wrapAll('<div class="js-accordion-body" />')
+  // })
+})
