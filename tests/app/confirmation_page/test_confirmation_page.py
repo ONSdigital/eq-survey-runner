abaeacfaa95aec07408afb99efb24af9dcf30e8a
iwootten <i.wootten@gmail.com>
2016-11-25 09:10:19 +0000

    Update var name in test for clarity

--- tests/app/confirmation_page/test_confirmation_page.py
+++ tests/app/confirmation_page/test_confirmation_page.py
@@ -7,7 +7,7 @@ import unittest
 class TestConfirmationPage(unittest.TestCase):
 
     def test_get_next_location_confirmation(self):
-        current_block_id = "5e633f04-073a-44c0-a9da-a7cc2116b937"
+        last_block_id = "5e633f04-073a-44c0-a9da-a7cc2116b937"
         answer = Answer(
             answer_id="ca3ce3a3-ae44-4e30-8f85-5b6a7a2fb23c",
             value="Orson Krennic",
@@ -16,6 +16,6 @@ class TestConfirmationPage(unittest.TestCase):
         answer_store.add(answer)
 
         navigator = Navigator(load_schema_file("0_rogue_one.json"), answer_store)
-        next_location = navigator.get_next_location(current_block_id=current_block_id)
+        next_location = navigator.get_next_location(current_block_id=last_block_id)
 
         self.assertEqual('summary', next_location['block_id'])
