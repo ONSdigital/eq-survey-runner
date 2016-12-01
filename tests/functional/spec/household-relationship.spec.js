5cb731658f30aca07962532912a62b8fdbd1b410
Ben Jefferies <benjefferies@echosoft.uk>
2016-12-01 12:00:03 +0000

    Fix selenium tests

--- tests/functional/spec/household-relationship.spec.js
+++ tests/functional/spec/household-relationship.spec.js
@@ -10,7 +10,7 @@ describe('Household relationship', function() {
 
   var schema = 'test_relationship_household.json'
 
-  it('Given I have added Joe Bloggs, Jane Doe and John Doe to my household when I am on the relationships page then I should see the questions `Joe Bloggs is the … of Jane Doe` and `Joe Bloggs is the … of John Doe`', function() {
+  it('Given I have added Joe Bloggs, Jane Doe and John Doe to my household when I set Joe Bloggs relationships then I should see the questions `Joe Bloggs is the husband or wife of Jane Doe` and `Joe Bloggs is the son or daughter of John Doe`', function() {
     // Given
     startQuestionnaire(schema)
     HouseholdCompositionPage.setPersonName(0, 'Joe Bloggs')
@@ -18,13 +18,15 @@ describe('Household relationship', function() {
       .setPersonName(1, 'Jane Doe')
       .addPerson()
       .setPersonName(2, 'John Doe')
+      .submit()
 
     // When
-    HouseholdCompositionPage.submit()
+    HouseholdRelationshipPage.setHusbandOrWifeRelationship(0)
+      .setSonOrDaughterRelationship(1)
 
     // Then
-    expect(HouseholdRelationshipPage.getRelationshipLabelAt(0)).to.have.string('Joe Bloggs is the … of Jane Doe')
-    expect(HouseholdRelationshipPage.getRelationshipLabelAt(1)).to.have.string('Joe Bloggs is the … of John Doe');
+    expect(HouseholdRelationshipPage.getRelationshipLabelAt(0)).to.have.string('Joe Bloggs is the husband or wife of Jane Doe')
+    expect(HouseholdRelationshipPage.getRelationshipLabelAt(1)).to.have.string('Joe Bloggs is the son or daughter of John Doe');
   })
 
   it('Given I have answered how Joe Bloggs is related Jane Doe and John Doe when I go to the next relationship question then I should see the questions `Jane Doe is the … of John Doe`', function() {
@@ -43,7 +45,7 @@ describe('Household relationship', function() {
       .submit()
 
     // Then
-    expect(HouseholdRelationshipPage.getRelationshipLabelAt(0)).to.have.string('Jane Doe is the … of John Doe')
+    expect(HouseholdRelationshipPage.getRelationshipLabelAt(0)).to.have.string('Jane Doe is the   …   of John Doe')
   })
 
   it('Given I am on the household page when I enter Joe Bloggs then I should not have to enter relationship details', function() {
