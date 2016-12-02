aa9c28cb9634d3475da27ea3f9b2f2db79d5c98c
Ben Jefferies <benjefferies@echosoft.uk>
2016-12-02 17:14:05 +0000

    Introduce versioned data converter
    * Added a new version of the answer converter to support repeating
    answering and groups.
    * Added data_version property in schema to support different converters
    
    [Trello card](https://trello.com/c/CRpuib4U/754-8-update-submitter-to-handle-latest-features)

--- app/submitter/converter.py
+++ app/submitter/converter.py
@@ -1,134 +1,157 @@
-import json
 import logging
+from collections import OrderedDict
 
 from datetime import datetime, timezone
 
 from app import settings
-from app.questionnaire_state.state_repeating_answer_question import extract_answer_instance_id
-
 
 logger = logging.getLogger(__name__)
 
 
-class SubmitterConstants(object):
+class DataVersionError(Exception):
+    def __init__(self, version):
+        self.version = version
 
-    PARADATA_KEY = "paradata"
-
-    METADATA_KEY = "metadata"
-
-    RU_REF_KEY = "ru_ref"
-
-    USER_ID_KEY = "user_id"
-
-    SUBMITTED_AT_KEY = "submitted_at"
-
-    COLLECTION_KEY = "collection"
-
-    PERIOD_KEY = "period"
-
-    EXERCISE_SID_KEY = "exercise_sid"
-
-    INSTRUMENT_KEY = "instrument_id"
-
-    SURVEY_ID_KEY = "survey_id"
-
-    ORIGIN_KEY = "origin"
-
-    VERSION_KEY = "version"
-
-    TYPE_KEY = "type"
-
-    DATA_KEY = "data"
-
-    TYPE = "uk.gov.ons.edc.eq:surveyresponse"
-
-    VERSION = "0.0.1"
-
-    ORIGIN = "uk.gov.ons.edc.eq"
-
-    TX_ID = "tx_id"
+    def __str__(self):
+        return 'Data version {} not supported'.format(self.version)
 
 
-class Converter(object):
+def convert_answers(metadata, questionnaire, answer_store, routing_path):
+    """
+    Create the JSON answer format for down stream processing
 
-    @staticmethod
-    def prepare_answers(metadata_store, questionnaire, answers):
-        """
-        Create the JSON answer format for down stream processing
+    :param metadata: metadata for the questionnaire
+    :param questionnaire: the questionnaire schema
+    :param answer_store: the users answers
+    :param routing_path: the path followed by the user when answering the questionnaire
+    :return: a JSON object in the following format:
+      {
+        "tx_id": "0f534ffc-9442-414c-b39f-a756b4adc6cb",
+        "type" : "uk.gov.ons.edc.eq:surveyresponse",
+        "version" : "0.0.1",
+        "origin" : "uk.gov.ons.edc.eq",
+        "survey_id": "021",
+        "collection":{
+          "exercise_sid": "hfjdskf",
+          "instrument_id": "yui789",
+          "period": "2016-02-01"
+        },
+        "submitted_at": "2016-03-07T15:28:05Z",
+        "metadata": {
+          "user_id": "789473423",
+          "ru_ref": "432423423423"
+        },
+        "data": {}
+      }
+    """
+    survey_id = questionnaire.survey_id
+    submitted_at = datetime.now(timezone.utc)
+    payload = {
+        'tx_id': metadata['tx_id'],
+        'type': 'uk.gov.ons.edc.eq:surveyresponse',
+        'version': questionnaire.data_version,
+        'origin': 'uk.gov.ons.edc.eq',
+        'survey_id': survey_id,
+        'submitted_at': submitted_at.isoformat(),
+        'collection': _build_collection(metadata),
+        'metadata': _build_metadata(metadata),
+    }
+    if questionnaire.data_version == '0.0.2':
+        payload['data'] = convert_answers_to_census_data(answer_store, routing_path)
+    elif questionnaire.data_version == '0.0.1':
+        payload['data'] = convert_answers_to_data(answer_store, questionnaire)
+    else:
+        raise DataVersionError(questionnaire.data_version)
 
-        :param metadata_store: the metadata store
-        :param questionnaire: the questionnaire schema
-        :param answers: the users answers as a dict of id/value
-        :return: a JSON object in the following format:
-          {
-            "tx_id": "0f534ffc-9442-414c-b39f-a756b4adc6cb",
-            "type" : "uk.gov.ons.edc.eq:surveyresponse",
-            "version" : "0.0.1",
-            "origin" : "uk.gov.ons.edc.eq",
-            "survey_id": "021",
-            "collection":{
-              "exercise_sid": "hfjdskf",
-              "instrument_id": "yui789",
-              "period": "2016-02-01"
-            },
-            "submitted_at": "2016-03-07T15:28:05Z",
-            "metadata": {
-              "user_id": "789473423",
-              "ru_ref": "432423423423"
-            },
-            "paradata": {},
-            "data": {
-              "001": "01-01-2016",
-              "002": "30-03-2016"
-            }
-          }
-        """
-        survey_id = questionnaire.survey_id
-        data = {}
+    logging.debug("Converted answer ready for submission: %s", metadata['tx_id'])
+    return payload
 
-        for key in answers.keys():
-            answer_id, _ = extract_answer_instance_id(key)
-            item = questionnaire.get_item_by_id(answer_id)
-            if item is not None:
-                value = answers[key]
-                if value is not None:
-                    if item.code not in data:
-                        data[item.code] = Converter._encode_value(value)
-                    else:
-                        if not isinstance(data[item.code], list):
-                            list_answers = [data[item.code]]
-                            data[item.code] = list_answers
-                        data[item.code].append(Converter._encode_value(value))
 
-        metadata = {SubmitterConstants.USER_ID_KEY: metadata_store['user_id'],
-                    SubmitterConstants.RU_REF_KEY: metadata_store['ru_ref']}
+def convert_answers_to_data(answer_store, questionnaire_schema):
+    """
+    Convert answers into the data format below
+    "data": {
+          "001": "01-01-2016",
+          "002": "30-03-2016"
+        }
+    :param answer_store: questionnaire answers
+    :param questionnaire_schema: Schema of the questionnaire answers
+    :return: data in a formatted form
+    """
+    data = OrderedDict()
+    for answer in answer_store.answers:
+        item = questionnaire_schema.get_item_by_id(answer['answer_id'])
+        value = answer['value']
+        if item is not None and value is not None:
+            if item.code not in data:
+                data[item.code] = _encode_value(value)
+            else:
+                if not isinstance(data[item.code], list):
+                    list_answers = [data[item.code]]
+                    data[item.code] = list_answers
+                data[item.code].append(_encode_value(value))
+    return data
 
-        collection = {SubmitterConstants.EXERCISE_SID_KEY: metadata_store['collection_exercise_sid'],
-                      SubmitterConstants.INSTRUMENT_KEY: metadata_store['form_type'],
-                      SubmitterConstants.PERIOD_KEY: metadata_store['period_id']}
 
-        paradata = {}
-        submitted_at = datetime.now(timezone.utc)
+def convert_answers_to_census_data(answer_store, routing_path):
+    """
+    Convert answers into the data format below
+    "data": [
+        {
+            "value": "Joe Bloggs",
+            "block_id": "household-composition",
+            "answer_id": "household-full-name",
+            "group_id": "multiple-questions-group",
+            "group_instance": 0,
+            "answer_instance": 0
+        },
+        {
+            "value": "Fred Flintstone",
+            "block_id": "household-composition",
+            "answer_id": "household-full-name",
+            "group_id": "multiple-questions-group",
+            "group_instance": 0,
+            "answer_instance": 1
+        },
+        {
+            "value": "Husband or wife",
+            "block_id": "relationships",
+            "answer_id": "who-is-related",
+            "group_id": "household-relationships",
+            "group_instance": 0,
+            "answer_instance": 0
+        }
+    ]
+    :param answer_store: questionnaire answers
+    :param routing_path: the path followed in the questionnaire
+    :return: data in a formatted form
+    """
+    data = []
+    for location in routing_path:
+        answers_in_block = answer_store.filter(group_id=location['group_id'], group_instance=location['group_instance'], block_id=location['block_id'])
+        data.extend(answers_in_block)
+    return data
 
-        payload = {SubmitterConstants.TX_ID: metadata_store['tx_id'],
-                   SubmitterConstants.TYPE_KEY: SubmitterConstants.TYPE,
-                   SubmitterConstants.VERSION_KEY: SubmitterConstants.VERSION,
-                   SubmitterConstants.ORIGIN_KEY: SubmitterConstants.ORIGIN,
-                   SubmitterConstants.SURVEY_ID_KEY: survey_id,
-                   SubmitterConstants.SUBMITTED_AT_KEY: submitted_at.isoformat(),
-                   SubmitterConstants.COLLECTION_KEY: collection,
-                   SubmitterConstants.METADATA_KEY: metadata,
-                   SubmitterConstants.PARADATA_KEY: paradata,
-                   SubmitterConstants.DATA_KEY: data}
 
-        logging.debug("Converted answer ready for submission %s", json.dumps(payload))
-        return payload, submitted_at
+def _build_collection(metadata):
+    return {
+        'exercise_sid': metadata['collection_exercise_sid'],
+        'instrument_id': metadata['form_type'],
+        'period': metadata['period_id'],
+    }
 
-    @staticmethod
-    def _encode_value(value):
-        if isinstance(value, int):
-            return str(value)
-        elif isinstance(value, datetime):
-            return value.strftime(settings.SDX_DATE_FORMAT)
-        else:
-            return value
+
+def _build_metadata(metadata):
+    return {
+        'user_id': metadata['user_id'],
+        'ru_ref': metadata['ru_ref'],
+    }
+
+
+def _encode_value(value):
+    if isinstance(value, int):
+        return str(value)
+    elif isinstance(value, datetime):
+        return value.strftime(settings.SDX_DATE_FORMAT)
+    else:
+        return value
