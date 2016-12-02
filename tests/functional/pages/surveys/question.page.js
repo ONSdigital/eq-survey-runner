ffa03ae8a46bd251d3ef4f41d15cb22adaf7d23c
jonnyshaw89 <jon.shaw@jassoft.co.uk>
2016-12-02 15:28:04 +0000

    Fixed validation text

--- tests/functional/pages/surveys/question.page.js
+++ tests/functional/pages/surveys/question.page.js
@@ -13,6 +13,10 @@ class QuestionPage {
     return browser.isExisting('.js-inpagelink')
   }
 
+  getErrorMsg() {
+    return browser.element('.js-inpagelink').getText()
+  }
+
   previous() {
     return browser.click('a[id="top-previous"]')
   }
