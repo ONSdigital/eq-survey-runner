0d437d046bcc65086d49369285456683ec93a783
Andrew Maddaford <ajmaddaford@gmail.com>
2016-12-07 16:11:39 +0000

    Update integration test for removing household composition answers

--- tests/integration/household_composition/test_repeating_household.py
+++ tests/integration/household_composition/test_repeating_household.py
@@ -11,125 +11,82 @@ class TestRepeatingHousehold(IntegrationTestCase):
     def setUp(self):
         super(TestRepeatingHousehold, self).setUp()
 
+        self.token = create_token('repeating_household', 'test')
+        self.client.get('/session?token=' + self.token.decode(), follow_redirects=True)
+        resp = self.client.post(
+            self.INTRODUCTION_PAGE,
+            data={'action[start_questionnaire]': 'Start Questionnaire'},
+            follow_redirects=False)
+        self.first_page = resp.headers['Location']
+
     def tearDown(self):
         super(TestRepeatingHousehold, self).tearDown()
 
-    def test_add_answer(self):
-        self.add_answers()
-
-    def test_remove_answer(self):
-        first_page = self.add_answers()
-        self.remove_answer(first_page)
-
-    def test_regular_submit(self):
-        first_page = self.add_answers()
-        self.submit_answers(first_page)
-
-    def submit_answers(self, page):
-        # Add first person
+    def test_submit_answers(self):
+        # Given I add some people
+        household_composition_page = self.first_page
         form_data = MultiDict()
         form_data.add("household-full-name", 'Person One')
         form_data.add("household-full-name_1", 'Person Two')
-        form_data.add("household-full-name_2", 'Person Three')
-        form_data.add("household-full-name_3", 'Person Four')
-        form_data.add("action[save_continue]", "") # Remove person two.
-        resp = self.client.post(page, data=form_data, follow_redirects=False)
-        self.assertEquals(resp.status_code, 302)
+        form_data.add("action[save_continue]", '')
+        resp = self.client.post(household_composition_page, data=form_data, follow_redirects=False)
+        person1_age_page = resp.headers['Location']
 
-        summary_page = resp.headers['Location']
-        self.assertNotEqual(page, summary_page)
+        # And provide detailsvi
+        self.navigate_to_page(person1_age_page)
+        form_data = MultiDict()
+        form_data.add("what-is-your-age", '9990')
+        form_data.add("action[save_continue]", '')
+        resp = self.client.post(person1_age_page, data=form_data, follow_redirects=False)
+        person1_shoe_size_page = resp.headers['Location']
 
-    def remove_answer(self, page):
-        # Add first person
+        self.navigate_to_page(person1_shoe_size_page)
+        form_data = MultiDict()
+        form_data.add("what-is-your-shoe-size", '9991')
+        form_data.add("action[save_continue]", '')
+        resp = self.client.post(person1_shoe_size_page, data=form_data, follow_redirects=False)
+        person2_age_page = resp.headers['Location']
+
+        self.navigate_to_page(person2_age_page)
+        form_data = MultiDict()
+        form_data.add("what-is-your-age", '9992')
+        form_data.add("action[save_continue]", '')
+        resp = self.client.post(person2_age_page, data=form_data, follow_redirects=False)
+        person2_shoe_size_page = resp.headers['Location']
+
+        self.navigate_to_page(person2_shoe_size_page)
+        form_data = MultiDict()
+        form_data.add("what-is-your-shoe-size", '9993')
+        form_data.add("action[save_continue]", '')
+        self.client.post(person2_shoe_size_page, data=form_data, follow_redirects=False)
+
+        # When I go back to household composition page and submit
+        self.navigate_to_page(household_composition_page)
         form_data = MultiDict()
         form_data.add("household-full-name", 'Person One')
         form_data.add("household-full-name_1", 'Person Two')
-        form_data.add("household-full-name_2", 'Person Three')
-        form_data.add("household-full-name_3", '')
-        form_data.add("action[remove_answer]", "1") # Remove person two.
-        resp = self.client.post(page, data=form_data, follow_redirects=False)
+        form_data.add("action[save_continue]", '')
+        resp = self.client.post(household_composition_page, data=form_data, follow_redirects=True)
 
-        self.assertEquals(resp.status_code, 200)
-
-        resp = self.navigate_to_page(page)
+        # Then the details previously entered for the people should have been removed
         content = resp.get_data(True)
-        self.assertRegex(content, 'household-full-name')
-        self.assertRegex(content, 'household-full-name_2')
-        self.assertRegex(content, 'household-full-name_3')
-
-        self.assertNotRegex(content, 'household-full-name_1')
-
-    def add_answers(self):
-        self.login_and_check_introduction_text()
-        first_page = self.start_questionnaire()
-
-        # Add first person
+        self.assertRegex(content, 'Person One')
+        self.assertRegex(content, 'What is their age')
+        self.assertNotRegex(content, '9990')
         form_data = MultiDict()
-        form_data.add("household-full-name", 'Person One')
-        form_data.add("action[add_answer]", "")
-        resp = self.client.post(first_page, data=form_data, follow_redirects=False)
+        form_data.add("what-is-your-age", '34')
+        form_data.add("action[save_continue]", '')
+        self.client.post(person1_age_page, data=form_data, follow_redirects=True)
 
-        self.assertEquals(resp.status_code, 200)
-
-        resp = self.navigate_to_page(first_page)
-        content = resp.get_data(True)
-        self.assertRegex(content, 'household-full-name_1')
-
-        # Add second person
         form_data = MultiDict()
-        form_data.add("household-full-name", 'Person One')
-        form_data.add("household-full-name_1", 'Person Two')
-        form_data.add("action[add_answer]", "")
-        resp = self.client.post(first_page, data=form_data, follow_redirects=False)
+        form_data.add("what-is-your-shoe-size", '10')
+        form_data.add("action[save_continue]", '')
+        resp = self.client.post(person1_shoe_size_page, data=form_data, follow_redirects=True)
 
-        self.assertEquals(resp.status_code, 200)
-
-        resp = self.navigate_to_page(first_page)
         content = resp.get_data(True)
-        self.assertRegex(content, 'household-full-name_1')
-        self.assertRegex(content, 'household-full-name_2')
-
-        # Add third person
-        form_data = MultiDict()
-        form_data.add("household-full-name", 'Person One')
-        form_data.add("household-full-name_1", 'Person Two')
-        form_data.add("household-full-name_2", 'Person Three')
-        form_data.add("action[add_answer]", "")
-        resp = self.client.post(first_page, data=form_data, follow_redirects=False)
-
-        self.assertEquals(resp.status_code, 200)
-
-        resp = self.navigate_to_page(first_page)
-        content = resp.get_data(True)
-        self.assertRegex(content, 'household-full-name_1')
-        self.assertRegex(content, 'household-full-name_2')
-        self.assertRegex(content, 'household-full-name_3')
-
-        return first_page
-
-    def login_and_check_introduction_text(self):
-        self.token = create_token('repeating_household', 'test')
-        resp = self.get_first_page()
-        self.check_introduction_text(resp)
-
-    def get_first_page(self):
-        resp = self.client.get('/session?token=' + self.token.decode(), follow_redirects=True)
-        self.assertEquals(resp.status_code, 200)
-        return resp
-
-    def check_introduction_text(self, response):
-        content = response.get_data(True)
-        self.assertRegex(content, '<li>Household questions.</li>')
-
-    def start_questionnaire(self):
-        # Go to questionnaire
-        post_data = {
-            'action[start_questionnaire]': 'Start Questionnaire'
-        }
-        resp = self.client.post(self.INTRODUCTION_PAGE, data=post_data, follow_redirects=False)
-        self.assertEquals(resp.status_code, 302)
-
-        return resp.headers['Location']
+        self.assertRegex(content, 'Person Two')
+        self.assertRegex(content, 'What is their age')
+        self.assertNotRegex(content, '9992')
 
     def navigate_to_page(self, page):
         resp = self.client.get(page, follow_redirects=False)
