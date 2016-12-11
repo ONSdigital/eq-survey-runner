bacb31842811293c2b230d3e5af435045bb4a8cf
Samiwel Thomas <samiwel.thomas@gmail.com>
2016-12-11 15:07:06 +0000

    Fixes #690 quotes in household member name causes error.

--- tests/app/templating/test_template_renderer.py
+++ tests/app/templating/test_template_renderer.py
@@ -26,3 +26,40 @@ class TestTemplateRenderer(unittest.TestCase):
         rendered = TemplateRenderer().render(date, **context)
 
         self.assertEqual(rendered, '1 January 2017')
+
+    def test_strings_containing_backslashes_are_escaped(self):
+        title = '{{ [answers.person_name] | format_household_name }}'
+        context = {
+            'answers': {
+                'person_name': '\\',
+            }
+        }
+
+        rendered = TemplateRenderer().render(title, **context)
+
+        self.assertEqual(rendered, '\\')
+
+    def test_strings_containing_quotes_are_escaped(self):
+        title = '{{ [answers.person_name] | format_household_name }}'
+        context = {
+            'answers': {
+                'person_name': '"',
+            }
+        }
+
+        rendered = TemplateRenderer().render(title, **context)
+
+        self.assertEqual(rendered, '"')
+
+    def test_household_summary_values_are_escaped(self):
+        description = "<h2 class='neptune'>Your household includes:</h2> {{ [answers.first_name, answers.middle_names, answers.last_name]|format_household_summary }}"
+        context = {
+            'answers': {
+                'first_name': ['Alice', 'Bob', '\\', 'Dave'],
+                'middle_names': ['', 'Berty', '"', 'Dixon'],
+                'last_name': ['Aardvark', 'Brown', '!', 'Davies'],
+            }
+        }
+
+        rendered = TemplateRenderer().render(description, **context)
+        self.assertEqual(rendered, '<h2 class=\'neptune\'>Your household includes:</h2> <ul><li>Alice Aardvark</li><li>Bob Berty Brown</li><li>\ " !</li><li>Dave Dixon Davies</li></ul>')
