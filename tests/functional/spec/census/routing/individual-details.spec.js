2c9c37b9a2eaf16efc986d22ad55068dec551d4f
Ben Jefferies <benjefferies@echosoft.uk>
2016-12-16 08:56:13 +0000

    Splitting routing questions into pages
    When a test fails it takes a long time to run all of the routing rules
    to see what has broken. It is also more difficult to maintain. It will
    only get larger and more unmaintainable if we don't split them out

---
+++ tests/functional/spec/census/routing/individual-details.spec.js
@@ -0,0 +1,103 @@
+import chai from 'chai'
+import {startCensusQuestionnaire} from '../../../helpers'
+
+import PermanentOrFamilyHome from '../../../pages/surveys/census/household/permanent-or-family-home.page.js'
+import ElsePermanentOrFamilyHome from '../../../pages/surveys/census/household/else-permanent-or-family-home.page.js'
+import HouseholdComposition from '../../../pages/surveys/census/household/household-composition.page.js'
+import EveryoneAtAddressConfirmation from '../../../pages/surveys/census/household/everyone-at-address-confirmation.page.js'
+import OvernightVisitors from '../../../pages/surveys/census/household/overnight-visitors.page.js'
+import HouseholdRelationships from '../../../pages/surveys/census/household/household-relationships.page.js'
+import WhoLivesHereCompleted from '../../../pages/surveys/census/household/who-lives-here-completed.page.js'
+import TypeOfAccommodation from '../../../pages/surveys/census/household/type-of-accommodation.page.js'
+import TypeOfHouse from '../../../pages/surveys/census/household/type-of-house.page.js'
+import SelfContainedAccommodation from '../../../pages/surveys/census/household/self-contained-accommodation.page.js'
+import NumberOfBedrooms from '../../../pages/surveys/census/household/number-of-bedrooms.page.js'
+import CentralHeating from '../../../pages/surveys/census/household/central-heating.page.js'
+import OwnOrRent from '../../../pages/surveys/census/household/own-or-rent.page.js'
+import NumberOfVehicles from '../../../pages/surveys/census/household/number-of-vehicles.page.js'
+import HouseholdAndAccommodationCompleted from '../../../pages/surveys/census/household/household-and-accommodation-completed.page.js'
+import HouseholdMemberBegin from '../../../pages/surveys/census/household/household-member-begin.page.js'
+import DetailsCorrect from '../../../pages/surveys/census/household/details-correct.page.js'
+import CorrectName from '../../../pages/surveys/census/household/correct-name.page.js'
+import Sex from '../../../pages/surveys/census/household/sex.page.js'
+import DateOfBirth from '../../../pages/surveys/census/household/date-of-birth.page.js'
+import Over16 from '../../../pages/surveys/census/household/over-16.page.js'
+import PrivateResponse from '../../../pages/surveys/census/household/private-response.page.js'
+import MaritalStatus from '../../../pages/surveys/census/household/marital-status.page.js'
+import AnotherAddress from '../../../pages/surveys/census/household/another-address.page.js'
+import OtherAddress from '../../../pages/surveys/census/household/other-address.page.js'
+import AddressType from '../../../pages/surveys/census/household/address-type.page.js'
+import InEducation from '../../../pages/surveys/census/household/in-education.page.js'
+import TermTimeLocation from '../../../pages/surveys/census/household/term-time-location.page.js'
+import CountryOfBirth from '../../../pages/surveys/census/household/country-of-birth.page.js'
+import Carer from '../../../pages/surveys/census/household/carer.page.js'
+import NationalIdentity from '../../../pages/surveys/census/household/national-identity.page.js'
+import EthnicGroup from '../../../pages/surveys/census/household/ethnic-group.page.js'
+import WhiteEthnicGroup from '../../../pages/surveys/census/household/white-ethnic-group.page.js'
+import Language from '../../../pages/surveys/census/household/language.page.js'
+import English from '../../../pages/surveys/census/household/english.page.js'
+import Religion from '../../../pages/surveys/census/household/religion.page.js'
+import PastUsualAddress from '../../../pages/surveys/census/household/past-usual-address.page.js'
+import Passports from '../../../pages/surveys/census/household/passports.page.js'
+import Disability from '../../../pages/surveys/census/household/disability.page.js'
+import Qualifications from '../../../pages/surveys/census/household/qualifications.page.js'
+import Volunteering from '../../../pages/surveys/census/household/volunteering.page.js'
+import EmploymentType from '../../../pages/surveys/census/household/employment-type.page.js'
+import HouseholdMemberCompleted from '../../../pages/surveys/census/household/household-member-completed.page.js'
+import Confirmation from '../../../pages/confirmation.page.js'
+
+const expect = chai.expect
+
+describe('Individual details routing Scenarios', function () {
+
+  it('Given I am answering question 1 in the individual detail section, When I select -no- as response, Then I am routed to What is your correct name question 1a ', function () {
+    startCensusQuestionnaire('census_household.json', true)
+    PermanentOrFamilyHome.clickPermanentOrFamilyHomeAnswerYes().submit()
+    HouseholdComposition.setFirstName('John').addPerson().setFirstName('Jane', 1).submit()
+    EveryoneAtAddressConfirmation.clickEveryoneAtAddressConfirmationAnswerYes().submit()
+    OvernightVisitors.setOvernightVisitorsAnswer(0).submit()
+    HouseholdRelationships.clickHouseholdRelationshipsAnswerHusbandOrWife().submit()
+    WhoLivesHereCompleted.submit()
+
+    // household-and-accommodation
+    TypeOfAccommodation.clickTypeOfAccommodationAnswerWholeHouseOrBungalow().submit()
+    TypeOfHouse.clickTypeOfHouseAnswerSemiDetached().submit()
+    SelfContainedAccommodation.clickSelfContainedAccommodationAnswerYesAllTheRoomsAreBehindADoorThatOnlyThisHouseholdCanUse().submit()
+    NumberOfBedrooms.setNumberOfBedroomsAnswer(3).submit()
+    CentralHeating.clickCentralHeatingAnswerGas().submit()
+    OwnOrRent.clickOwnOrRentAnswerOwnsOutright().submit()
+    NumberOfVehicles.setNumberOfVehiclesAnswer(2).submit()
+    HouseholdAndAccommodationCompleted.submit()
+
+    // household-member
+    HouseholdMemberBegin.submit()
+    DetailsCorrect.clickDetailsCorrectAnswerNoINeedToChangeMyName().submit()
+    expect(CorrectName.isOpen()).to.equal(true, 'Expecting correct name page to be open')
+  })
+
+  it('Given I am answering question 1 in the individual detail section, When I do not select any response, Then I am routed to Are you over 16', function () {
+    startCensusQuestionnaire('census_household.json', true)
+    PermanentOrFamilyHome.clickPermanentOrFamilyHomeAnswerYes().submit()
+    HouseholdComposition.setFirstName('John').addPerson().setFirstName('Jane', 1).submit()
+    EveryoneAtAddressConfirmation.clickEveryoneAtAddressConfirmationAnswerYes().submit()
+    OvernightVisitors.setOvernightVisitorsAnswer(0).submit()
+    HouseholdRelationships.clickHouseholdRelationshipsAnswerHusbandOrWife().submit()
+    WhoLivesHereCompleted.submit()
+
+    // household-and-accommodation
+    TypeOfAccommodation.clickTypeOfAccommodationAnswerWholeHouseOrBungalow().submit()
+    TypeOfHouse.clickTypeOfHouseAnswerSemiDetached().submit()
+    SelfContainedAccommodation.clickSelfContainedAccommodationAnswerYesAllTheRoomsAreBehindADoorThatOnlyThisHouseholdCanUse().submit()
+    NumberOfBedrooms.setNumberOfBedroomsAnswer(3).submit()
+    CentralHeating.clickCentralHeatingAnswerGas().submit()
+    OwnOrRent.clickOwnOrRentAnswerOwnsOutright().submit()
+    NumberOfVehicles.setNumberOfVehiclesAnswer(2).submit()
+    HouseholdAndAccommodationCompleted.submit()
+
+    // household-member
+    HouseholdMemberBegin.submit()
+    DetailsCorrect.submit()
+    expect(Over16.isOpen()).to.equal(true, 'Expecting over 16 page to be open')
+  })
+
+})
