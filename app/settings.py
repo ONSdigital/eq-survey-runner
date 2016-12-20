d8acef2f1164cb36b88a476a3f2e4ef92549d0d0
Ben Jefferies <benjefferies@echosoft.uk>
2016-12-20 22:06:33 +0000

    Removing logic to load schemas from s3
    The app fails to start up if you don't have the EQ_SCHEMA_BUCKET.
    We don't currently utilise s3 for loading schemas therefore we can
    remove the logic until the time it is needed.

--- app/settings.py
+++ app/settings.py
@@ -48,7 +48,6 @@ EQ_SCHEMA_DIRECTORY = os.getenv('EQ_SCHEMA_DIRECTORY', 'app/data')
 EQ_SESSION_TIMEOUT = int(os.getenv('EQ_SESSION_TIMEOUT', '28800'))
 EQ_SECRET_KEY = os.getenv('EQ_SECRET_KEY', os.urandom(24))
 EQ_UA_ID = os.getenv('EQ_UA_ID', '')
-EQ_SCHEMA_BUCKET = os.getenv('EQ_SCHEMA_BUCKET', "eq-schema-files-" + os.getenv('USER', 'UNKNOWN'))
 EQ_MAX_REPLAY_COUNT = os.getenv('EQ_REPLAY_COUNT', 50)
 
 
