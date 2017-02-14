68bc006ffee9e654357242166b63406a345c799b
iwootten <i.wootten@gmail.com>
2017-02-14 11:43:38 +0000

    Remove updated empty answers

--- tests/functional/spec/summary-screen.spec.js
+++ tests/functional/spec/summary-screen.spec.js
@@ -6,7 +6,9 @@ import retailTurnoverPage from '../pages/surveys/rsi/0102/retail-turnover.page'
 import internetSalesPage from '../pages/surveys/rsi/0102/internet-sales.page'
 import changeInRetailTurnover from '../pages/surveys/rsi/0102/changes-in-retail-turnover.page'
 import rsiSummaryPage from '../pages/surveys/rsi/0102/rsi-summary.page'
-import SummaryPage from '../pages/summary.page'
+import employeesPage from '../pages/surveys/rsi/0112/employees.page'
+import changesInEmployeesPage from '../pages/surveys/rsi/0112/changes-in-employees.page'
+import rsiWithEmployeesSummaryPage from '../pages/surveys/rsi/0112/rsi-summary.page'
 
 const expect = chai.expect
 
@@ -39,9 +41,9 @@ describe('RSI - summary screen edit test', function() {
     expect(rsiSummaryPage.getChangeInRetailTurnoverSummary()).to.contain('This is to test edit links on summary screen')
   })
 
-  it('Given the RSI survery 0102 is saved with answers when edit link is clicked then it should allow to edit the answer ', function(done) {
+  it('Given the RSI survey 0102 is saved with answers when edit link is clicked then it should allow to edit the answer ', function(done) {
 
-    //Given the RSI survery 0102 is saved with answers
+    //Given the RSI survey 0102 is saved with answers
     startQuestionnaire('1_0102.json')
     reportingPeriod.setFromReportingPeriodDay(2)
       .setToReportingPeriodDay(2)
@@ -67,6 +69,7 @@ describe('RSI - summary screen edit test', function() {
     expect(rsiSummaryPage.getChangeInRetailTurnoverSummary()).to.contain('This is to test edit links on summary screen - edited')
 
   })
+
   it('Given the RSI survey 0102 when a 0 is entered in a currency field then the summary screen should show £0 and the original page should show 0', function(done) {
 
     //Given the RSI business survey 0102 is started
@@ -92,6 +95,47 @@ describe('RSI - summary screen edit test', function() {
     expect(rsiSummaryPage.getInternetSalesSummary()).to.contain('£' + 0)
     rsiSummaryPage.editLinkChangeInternetSales()
     expect(internetSalesPage.getInternetSales()).to.contain('0')
+
+  })
+
+  it('Given the RSI survey 0112 when a non mandatory field is edited to contain no answer, the summary page should display "No answer provided"', function(done) {
+
+    startQuestionnaire('1_0112.json')
+
+    // when data is entered for the survey
+    reportingPeriod.setFromReportingPeriodDay(2)
+      .setToReportingPeriodDay(2)
+      .setFromReportingPeriodMonth(5)
+      .setToReportingPeriodMonth(5)
+      .setFromReportingPeriodYear(2016)
+      .setToReportingPeriodYear(2017)
+      .submit()
+    retailTurnoverPage.setRetailTurnover(1000)
+      .submit()
+    internetSalesPage.setInternetSales(0)
+      .submit()
+    changeInRetailTurnover.setChangesInRetailTurnover('')
+      .submit()
+    employeesPage.setMaleEmployeesOver30Hours(2)
+        .setTotalEmployees(2)
+        .submit()
+    changesInEmployeesPage.setChangesInEmployeesPage('')
+        .submit()
+
+    // Then summary screen and the original page shows the data entered
+    expect(rsiWithEmployeesSummaryPage.getRetailTurnoverSummary()).to.contain('£1,000')
+    expect(rsiWithEmployeesSummaryPage.getInternetSalesSummary()).to.contain('£' + 0)
+    expect(rsiWithEmployeesSummaryPage.getChangeMaleEmployeesOver30Hours()).to.contain('2')
+
+    // and when we edit the total male employees
+    rsiWithEmployeesSummaryPage.editLinkChangeMaleEmployeesOver30Hours()
+    employeesPage.setMaleEmployeesOver30Hours('')
+        .submit()
+    changesInEmployeesPage.setChangesInEmployeesPage('')
+        .submit()
+
+    // Then the summary screen should show the updated answer with no response
+    expect(rsiWithEmployeesSummaryPage.getChangeMaleEmployeesOver30Hours()).to.contain('No answer provided')
 
   })
 })
