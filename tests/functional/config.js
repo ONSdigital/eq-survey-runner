f3564f13edac03cd60f5e8fe20271003fbc3714d
Christopher Collis <christopher.collis@gmail.com>
2017-02-17 17:36:48 +0000

    Fix config

--- tests/functional/config.js
+++ tests/functional/config.js
@@ -78,7 +78,7 @@ const browserStackConfig = {
     build: 'master 2',
     project: 'EQ Survey Runner',
     'browserstack.local': true,
-    'browserstack.localIdentifier' = process.env.BROWSERSTACK_LOCAL_IDENTIFIER
+    'browserstack.localIdentifier': process.env.BROWSERSTACK_LOCAL_IDENTIFIER
   }],
   maxInstances: 4,
   framework: 'mocha',
