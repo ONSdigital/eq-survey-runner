c02bb7b917ca4437391a2b66677ea938656a0723
dcdarrell9 <dcdarrell9@users.noreply.github.com>
2017-04-11 14:06:28 +0100

    added unit tests in user.py and increase coverage

--- tests/app/authentication/test_user.py
+++ tests/app/authentication/test_user.py
@@ -22,3 +22,18 @@ class TestUser(unittest.TestCase):
         with self.application.test_request_context():
             user = User("1", "2")
             self.assertEqual("2", user.user_ik)
+
+    def test_negative_user(self):
+        with self.application.test_request_context():
+            user = User("-1", "2")
+            self.assertEqual("-1", user.user_id)
+
+    def test_no_user(self):
+        with self.application.test_request_context():
+            with self.assertRaises(ValueError):
+                User("", "")
+
+    def test_none_user(self):
+        with self.application.test_request_context():
+            with self.assertRaises(ValueError):
+                User(None, "2")
