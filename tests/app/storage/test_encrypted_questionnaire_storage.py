58dc405c0dc88fe9bcb42c6e6b2fabf8f4b0302a
Samiwel Thomas <samiwel.thomas@gmail.com>
2017-05-09 10:36:09 +0100

    Remove base class and pull methods into encrypted storage.

--- tests/app/storage/test_encrypted_questionnaire_storage.py
+++ tests/app/storage/test_encrypted_questionnaire_storage.py
@@ -1,45 +1,45 @@
 import unittest
 
-from app.storage.encrypted_questionnaire_storage import EncryptedQuestionnaireStorage, generate_key
+from app.storage.encrypted_questionnaire_storage import EncryptedQuestionnaireStorage
 
 
 class TestEncryptedQuestionnaireStorage(unittest.TestCase):
 
     def test_encrypted_storage_requires_user_id(self):
         with self.assertRaises(ValueError):
-            EncryptedQuestionnaireStorage(None, "key", 'pepper')
+            EncryptedQuestionnaireStorage(None, "key", "pepper")
 
     def test_encrypted_storage_requires_user_ik(self):
         with self.assertRaises(ValueError):
-            EncryptedQuestionnaireStorage("1", None, 'pepper')
+            EncryptedQuestionnaireStorage("1", None, "pepper")
 
     def test_generate_cek(self):
-        cek1 = generate_key("user1", "user_ik_1", "pepper")
-        cek2 = generate_key("user1", "user_ik_1", "pepper")
-        cek3 = generate_key("user2", "user_ik_2", "pepper")
+        cek1 = EncryptedQuestionnaireStorage("user1", "user_ik_1", "pepper").generate_key()
+        cek2 = EncryptedQuestionnaireStorage("user1", "user_ik_1", "pepper").generate_key()
+        cek3 = EncryptedQuestionnaireStorage("user2", "user_ik_2", "pepper").generate_key()
         self.assertEqual(cek1, cek2)
         self.assertNotEqual(cek1, cek3)
         self.assertNotEqual(cek2, cek3)
 
     def test_generate_cek_different_user_ids(self):
-        cek1 = generate_key("user1", "user_ik_1", "pepper")
-        cek2 = generate_key("user1", "user_ik_1", "pepper")
-        cek3 = generate_key("user2", "user_ik_1", "pepper")
+        cek1 = EncryptedQuestionnaireStorage("user1", "user_ik_1", "pepper").generate_key()
+        cek2 = EncryptedQuestionnaireStorage("user1", "user_ik_1", "pepper").generate_key()
+        cek3 = EncryptedQuestionnaireStorage("user2", "user_ik_1", "pepper").generate_key()
         self.assertEqual(cek1, cek2)
         self.assertNotEqual(cek1, cek3)
         self.assertNotEqual(cek2, cek3)
 
     def test_generate_cek_different_user_iks(self):
-        cek1 = generate_key("user1", "user_ik_1", "pepper")
-        cek2 = generate_key("user1", "user_ik_1", "pepper")
-        cek3 = generate_key("user1", "user_ik_2", "pepper")
+        cek1 = EncryptedQuestionnaireStorage("user1", "user_ik_1", "pepper").generate_key()
+        cek2 = EncryptedQuestionnaireStorage("user1", "user_ik_1", "pepper").generate_key()
+        cek3 = EncryptedQuestionnaireStorage("user1", "user_ik_2", "pepper").generate_key()
         self.assertEqual(cek1, cek2)
         self.assertNotEqual(cek1, cek3)
         self.assertNotEqual(cek2, cek3)
 
     def test_generate_cek_different_pepper(self):
-        cek1 = generate_key("user1", "user_ik_1", "pepper")
-        cek2 = generate_key("user1", "user_ik_1", "test")
+        cek1 = EncryptedQuestionnaireStorage("user1", "user_ik_1", "pepper").generate_key()
+        cek2 = EncryptedQuestionnaireStorage("user1", "user_ik_1", "test").generate_key()
         self.assertNotEqual(cek1, cek2)
 
     def test_store_and_get(self):
--- tests/app/storage/test_questionnaire_storage.py
+++ tests/app/storage/test_questionnaire_storage.py
@@ -4,27 +4,27 @@ from mock import Mock, patch
 from sqlalchemy.exc import IntegrityError
 
 from app import settings
-from app.storage.questionnaire_storage import QuestionnaireStorage
+from app.storage.encrypted_questionnaire_storage import EncryptedQuestionnaireStorage
 
 
 class TestQuestionnaireStorage(unittest.TestCase):
 
     def setUp(self):
         settings.EQ_SERVER_SIDE_STORAGE_DATABASE_URL = "sqlite://"
-        self.storage = QuestionnaireStorage("1")
+        self.storage = EncryptedQuestionnaireStorage("user_id", "user_ik", "pepper")
 
     def test_store(self):
-        data = {'test': 'test'}
+        data = 'test'
         self.assertIsNone(self.storage.add_or_update(data))
         self.assertIsNotNone(self.storage._get())  # pylint: disable=protected-access
 
     def test_get(self):
-        data = {'test': 'test'}
+        data = 'test'
         self.storage.add_or_update(data)
         self.assertEqual(data, self.storage.get_user_data())
 
     def test_delete(self):
-        data = {'test': 'test'}
+        data = 'test'
         self.storage.add_or_update(data)
         self.assertEqual(data, self.storage.get_user_data())
         self.storage.delete()
@@ -32,9 +32,9 @@ class TestQuestionnaireStorage(unittest.TestCase):
 
     def test_store_rollback(self):
         # Given
-        data = {'test': 'test'}
+        data = 'test'
 
-        with patch('app.storage.questionnaire_storage.db_session', autospec=True) as db_session:
+        with patch('app.storage.encrypted_questionnaire_storage.db_session', autospec=True) as db_session:
             db_session.commit.side_effect = IntegrityError(Mock(), Mock(), Mock())
 
             # When
@@ -48,10 +48,10 @@ class TestQuestionnaireStorage(unittest.TestCase):
 
     def test_delete_rollback(self):
         # Given
-        data = {'test': 'test'}
+        data = 'test'
         self.storage.add_or_update(data)
 
-        with patch('app.storage.questionnaire_storage.db_session', autospec=True) as db_session:
+        with patch('app.storage.encrypted_questionnaire_storage.db_session', autospec=True) as db_session:
             db_session.commit.side_effect = IntegrityError(Mock(), Mock(), Mock())
 
             # When
