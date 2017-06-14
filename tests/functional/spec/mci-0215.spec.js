4e3dfcc068efecc0462210b862fb1abccc4848cd
Andrew Maddaford <ajmaddaford@gmail.com>
2017-06-14 09:31:25 +0100

    Replace MCI (0215) survey with refresh

--- tests/functional/spec/mci-0215.spec.js
+++ tests/functional/spec/mci-0215.spec.js
@@ -2,22 +2,22 @@ import {startQuestionnaire} from '../helpers'
 import {openQuestionnaire} from '../helpers'
 
 import landingPage from '../pages/landing.page'
-import Introduction from '../pages/surveys/mci-refresh/introduction.page.js'
-import ReportingPeriod from '../pages/surveys/mci-refresh/reporting-period.page.js'
-import TotalRetailTurnoverBlock from '../pages/surveys/mci-refresh/total-retail-turnover-block.page.js'
-import FoodSales from '../pages/surveys/mci-refresh/food-sales.page.js'
-import AlcoholSales from '../pages/surveys/mci-refresh/alcohol-sales.page.js'
-import ClothingSales from '../pages/surveys/mci-refresh/clothing-sales.page.js'
-import HouseholdGoodsSales from '../pages/surveys/mci-refresh/household-goods-sales.page.js'
-import OtherGoodsSales from '../pages/surveys/mci-refresh/other-goods-sales.page.js'
-import TotalInternetSales from '../pages/surveys/mci-refresh/total-internet-sales.page.js'
-import AutomotiveFuel from '../pages/surveys/mci-refresh/automotive-fuel.page.js'
-import SignificantChange from '../pages/surveys/mci-refresh/significant-change.page.js'
-import ReasonForChange from '../pages/surveys/mci-refresh/reason-for-change.page.js'
-import ChangeCommentBlock from '../pages/surveys/mci-refresh/change-comment-block.page.js'
-import TotalEmployees from '../pages/surveys/mci-refresh/total-employees.page.js'
-import EmployeesBreakdown from '../pages/surveys/mci-refresh/employees-breakdown.page.js'
-import Summary from '../pages/surveys/mci-refresh/summary.page.js'
+import Introduction from '../pages/surveys/mci-0215/introduction.page.js'
+import ReportingPeriod from '../pages/surveys/mci-0215/reporting-period.page.js'
+import TotalRetailTurnoverBlock from '../pages/surveys/mci-0215/total-retail-turnover-block.page.js'
+import FoodSales from '../pages/surveys/mci-0215/food-sales.page.js'
+import AlcoholSales from '../pages/surveys/mci-0215/alcohol-sales.page.js'
+import ClothingSales from '../pages/surveys/mci-0215/clothing-sales.page.js'
+import HouseholdGoodsSales from '../pages/surveys/mci-0215/household-goods-sales.page.js'
+import OtherGoodsSales from '../pages/surveys/mci-0215/other-goods-sales.page.js'
+import TotalInternetSales from '../pages/surveys/mci-0215/total-internet-sales.page.js'
+import AutomotiveFuel from '../pages/surveys/mci-0215/automotive-fuel.page.js'
+import SignificantChange from '../pages/surveys/mci-0215/significant-change.page.js'
+import ReasonForChange from '../pages/surveys/mci-0215/reason-for-change.page.js'
+import ChangeCommentBlock from '../pages/surveys/mci-0215/change-comment-block.page.js'
+import TotalEmployees from '../pages/surveys/mci-0215/total-employees.page.js'
+import EmployeesBreakdown from '../pages/surveys/mci-0215/employees-breakdown.page.js'
+import Summary from '../pages/surveys/mci-0215/summary.page.js'
 import thankYou from '../pages/thank-you.page'
 
 
@@ -26,7 +26,7 @@ describe('MCI 0215 Test', function() {
   it('Given the mci business survey 0215 is selected when I start the survey then the landing page is displayed', function() {
     // Given
     // When
-    openQuestionnaire('mci_refresh.json')
+    openQuestionnaire('1_0215.json')
 
     // Then
     expect(landingPage.isOpen(), 'Landing page should be open').to.be.true
@@ -34,7 +34,7 @@ describe('MCI 0215 Test', function() {
 
   it('Given the mci business survey 0215 has been started when I complete the survey then I reach the thank you page', function() {
     // Given
-    startQuestionnaire('mci_refresh.json')
+    startQuestionnaire('1_0215.json')
 
     // When
     ReportingPeriod.setPeriodFromDay('01')
@@ -95,7 +95,7 @@ describe('MCI 0215 Test', function() {
 
   it('Given the mci business survey 0215 has been started and sales questions completed when I select No for significant changes I skip to total employees question', function() {
     // Given
-    startQuestionnaire('mci_refresh.json')
+    startQuestionnaire('1_0215.json')
 
     ReportingPeriod.setPeriodFromDay('01')
         .setPeriodFromMonth(5)
