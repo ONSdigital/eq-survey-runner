3a8374f60597f462b253939d66dce0a736405cb1
dwyeradam <adam.dwyer@ons.gov.uk>
2017-09-28 11:42:12 +0100

    Migrate additional functional tests
    Migrate remaining tests not related to Census or UKIS
    Remove no longer used functional tests specs and pages

---
+++ tests/new_functional/pages/surveys/timeout/timeout-block.page.js
@@ -0,0 +1,17 @@
+// >>> WARNING THIS PAGE WAS AUTO-GENERATED - DO NOT EDIT!!! <<<
+const QuestionPage = require('../question.page');
+
+class TimeoutBlockPage extends QuestionPage {
+
+  constructor() {
+    super('timeout-block');
+  }
+
+  answer() {
+    return '#timeout-answer';
+  }
+
+  answerLabel() { return '#label-timeout-answer'; }
+
+}
+module.exports = new TimeoutBlockPage();
---
+++ tests/new_functional/spec/total-breakdown.spec.js
@@ -0,0 +1,113 @@
+const helpers = require('../helpers');
+
+const BreakDownPage = require('../pages/surveys/total_breakdown/block.page.js');
+
+describe('Example Test', function() {
+
+  it('Given four percentage fields, When I enter 10, 20, 30 and 40 in the field, Then total should be 100', function() {
+    return helpers.openQuestionnaire('test_total_breakdown.json').then(() => {
+      return browser
+        .setValue(BreakDownPage.percentage1(), '10')
+        .setValue(BreakDownPage.percentage2(), '20')
+        .setValue(BreakDownPage.percentage3(), '30')
+        .setValue(BreakDownPage.percentage4(), '40')
+        .click(BreakDownPage.totalPercentageLabel())
+        .getValue(BreakDownPage.totalPercentage()).should.eventually.contain('100');
+    });
+  });
+
+  it('Given four percentage fields, When I enter non integer value into a field, Then total should ignore the non integer value', function() {
+    return helpers.openQuestionnaire('test_total_breakdown.json').then(() => {
+      return browser
+        .setValue(BreakDownPage.percentage1(), 'ten')
+        .setValue(BreakDownPage.percentage2(), '20')
+        .setValue(BreakDownPage.percentage3(), '30')
+        .setValue(BreakDownPage.percentage4(), '40')
+        .click(BreakDownPage.totalPercentageLabel())
+        .getValue(BreakDownPage.totalPercentage()).should.eventually.contain('90');
+    });
+  });
+
+  it('Given four percentage fields, When I enter a negative value into a field, Then total should ignore the negative value', function() {
+    return helpers.openQuestionnaire('test_total_breakdown.json').then(() => {
+      return browser
+        .setValue(BreakDownPage.percentage1(), '-10')
+        .setValue(BreakDownPage.percentage2(), '20')
+        .setValue(BreakDownPage.percentage3(), '30')
+        .setValue(BreakDownPage.percentage4(), '40')
+        .click(BreakDownPage.totalPercentageLabel())
+        .getValue(BreakDownPage.totalPercentage()).should.eventually.contain('90');
+    });
+  });
+
+  it('Given four percentage fields, When I enter a values totalling > 100, Then total should display the value', function() {
+  return helpers.openQuestionnaire('test_total_breakdown.json').then(() => {
+    return browser
+      .setValue(BreakDownPage.percentage1(), '20')
+      .setValue(BreakDownPage.percentage2(), '30')
+      .setValue(BreakDownPage.percentage3(), '40')
+      .setValue(BreakDownPage.percentage4(), '50')
+      .click(BreakDownPage.totalPercentageLabel())
+      .getValue(BreakDownPage.totalPercentage()).should.eventually.contain('140');
+    });
+  });
+
+  it('Given four percentage fields, When I enter non-integer values into each field, Then total should be 0', function() {
+  return helpers.openQuestionnaire('test_total_breakdown.json').then(() => {
+    return browser
+      .setValue(BreakDownPage.percentage1(), 'ten')
+      .setValue(BreakDownPage.percentage2(), 'twenty')
+      .setValue(BreakDownPage.percentage3(), 'thirty')
+      .setValue(BreakDownPage.percentage4(), 'forty')
+      .click(BreakDownPage.totalPercentageLabel())
+      .getValue(BreakDownPage.totalPercentage()).should.eventually.contain('0');
+    });
+  });
+
+  it('Given four percentage fields, When total is not 100, Then total field should be highlighted', function() {
+  return helpers.openQuestionnaire('test_total_breakdown.json').then(() => {
+    return browser
+      .setValue(BreakDownPage.percentage1(), '1')
+      .setValue(BreakDownPage.percentage2(), '2')
+      .setValue(BreakDownPage.percentage3(), '3')
+      .setValue(BreakDownPage.percentage4(), '4')
+      .click(BreakDownPage.totalPercentageLabel())
+      .isExisting(BreakDownPage.highlightedTotal())
+
+      .setValue(BreakDownPage.percentage4(), '100')
+      .click(BreakDownPage.totalPercentageLabel())
+      .isExisting(BreakDownPage.highlightedTotal())
+
+      .setValue(BreakDownPage.percentage4(), '94')
+      .click(BreakDownPage.totalPercentageLabel())
+      .isExisting(BreakDownPage.highlightedTotal()).should.eventually.be.false;
+    });
+  });
+
+  it('Given four percentage fields, When floating point numbers entered, Then total should be integer', function() {
+  return helpers.openQuestionnaire('test_total_breakdown.json').then(() => {
+    return browser
+      .setValue(BreakDownPage.percentage1(), '1.234')
+      .setValue(BreakDownPage.percentage2(), '2.345')
+      .setValue(BreakDownPage.percentage3(), '3.456')
+      .setValue(BreakDownPage.percentage4(), '4.567')
+      .click(BreakDownPage.totalPercentageLabel())
+      .getValue(BreakDownPage.totalPercentage()).should.eventually.contain('10');
+    });
+  });
+
+  it('Given a total field, When I navigate away then come back, Then total value should be preserved', function() {
+  return helpers.openQuestionnaire('test_total_breakdown.json').then(() => {
+    return browser
+      .setValue(BreakDownPage.percentage1(), '1')
+      .setValue(BreakDownPage.percentage2(), '2')
+      .setValue(BreakDownPage.percentage3(), '3')
+      .setValue(BreakDownPage.percentage4(), '4')
+      .click(BreakDownPage.submit())
+      .click(BreakDownPage.previous())
+      .getValue(BreakDownPage.totalPercentage()).should.eventually.contain('10');
+    });
+  });
+
+});
+
