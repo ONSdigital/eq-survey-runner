b3bf7b60247b1035b82403a06b6d0cb99bc0bbd2
Jamie Cox <me@jcox.tech>
2017-09-28 11:30:14 +0100

    Remove unused session helper function
    Session helper end_session_with_schema_context was necessary before template function refactor to add theme context to a session but is now redundant and has been removed.
    Was also causing an issue when attempting to do stuff with empty metadata (after a submission).
    
    Renames helper method to remove underscore.
    
    Fixes #1266

--- app/helpers/session_helper.py
+++ app/helpers/session_helper.py
@@ -1,20 +1,8 @@
 from flask import current_app
-from flask_login import current_user, logout_user
-
-from app.globals import get_questionnaire_store
-from app.utilities.schema import load_schema_from_metadata
+from flask_login import logout_user
 
 
-def _remove_survey_session_data():
+def remove_survey_session_data():
     current_app.eq['session_storage'].delete_session_from_db()
     current_app.eq['session_storage'].remove_user_ik()
     logout_user()
-
-
-def end_session_with_schema_context(schema=None):
-    if schema is None:
-        questionnaire_store = get_questionnaire_store(current_user.user_id, current_user.user_ik)
-        metadata = questionnaire_store.metadata
-        schema = load_schema_from_metadata(metadata)
-
-    _remove_survey_session_data()
