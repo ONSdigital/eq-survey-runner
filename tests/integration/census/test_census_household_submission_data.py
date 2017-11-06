bf778ea4393573552fbfb5db6b88cd4de683adbe
Mohammad Islam <Mohammad.Islam@ons.gov.uk>
2017-11-06 09:32:00 +0000

    intial commit

--- tests/integration/census/test_census_household_submission_data.py
+++ tests/integration/census/test_census_household_submission_data.py
@@ -416,33 +416,18 @@ class TestCensusHouseholdSubmissionData(IntegrationTestCase):
             },
             {
                 'group_instance': 0,
-                'answer_id': 'religion-welsh-answer-other',
-                'block_id': 'religion',
-                'group_id': 'household-member',
-                'answer_instance': 0,
-                'value': ''
-            },
-            {
-                'group_instance': 0,
-                'answer_id': 'religion-welsh-answer',
-                'block_id': 'religion',
-                'group_id': 'household-member',
-                'answer_instance': 0,
-                'value': []
-            },
-            {
-                'group_instance': 0,
                 'answer_id': 'religion-answer',
                 'block_id': 'religion',
                 'group_id': 'household-member',
                 'answer_instance': 0,
                 'value': [
                     'No religion',
-                    'Buddhist',
-                    'Hindu',
-                    'Jewish',
-                    'Muslim',
-                    'Sikh',
+                    'Christianity',
+                    'Buddhism',
+                    'Hinduism',
+                    'Judaism',
+                    'Islam',
+                    'Sikhism',
                     'Other'
                 ]
             },
@@ -981,11 +966,12 @@ class TestCensusHouseholdSubmissionData(IntegrationTestCase):
             },
             {
                 'religion-answer': ['No religion',
-                                    'Buddhist',
-                                    'Hindu',
-                                    'Jewish',
-                                    'Muslim',
-                                    'Sikh',
+                                    'Christianity',
+                                    'Buddhism',
+                                    'Hinduism',
+                                    'Judaism',
+                                    'Islam',
+                                    'Sikhism',
                                     'Other'],
                 'religion-answer-other': 'Ind'
             },
