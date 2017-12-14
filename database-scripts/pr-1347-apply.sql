a8f5f32732a62498f2d606b6b11ed9bfbb8ea52e
jonnyshaw89 <jon.shaw@jassoft.co.uk>
2017-12-14 12:18:45 +0000

    Correct datetime column to TIMESTAMP for postgres  - Postgres does not support a column type of DATETIME so this has been updated to be TIMESTAMP

--- database-scripts/pr-1347-apply.sql
+++ database-scripts/pr-1347-apply.sql
@@ -1,8 +1,8 @@
 ALTER TABLE questionnaire_state
-  ADD COLUMN created_at DATETIME;
+  ADD COLUMN created_at TIMESTAMP;
 
 ALTER TABLE questionnaire_state
-  ADD COLUMN updated_at DATETIME;
+  ADD COLUMN updated_at TIMESTAMP;
 
 ALTER TABLE questionnaire_state
   ADD COLUMN version INT;
