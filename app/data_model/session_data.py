35f52353daabfd9967157e0334efb8b9c0ae8b11
dwyeradam <adam.dwyer@ons.gov.uk>
2018-02-09 16:27:42 +0000

    Add future variables to session_data

--- app/data_model/session_data.py
+++ app/data_model/session_data.py
@@ -1,6 +1,6 @@
 class SessionData(object):
 
-    def __init__(self, tx_id, eq_id, form_type, period_str, ru_name=None, ru_ref=None, submitted_time=None):
+    def __init__(self, tx_id, eq_id, form_type, period_str, language_code=None, survey_url=None, ru_name=None, ru_ref=None, submitted_time=None):
         self.tx_id = tx_id
         self.eq_id = eq_id
         self.form_type = form_type
