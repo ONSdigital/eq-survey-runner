0f0937ec83796a23081d4ee4eba6c87b40f33598
Mebin Abraham <mebin.abraham@qa.com>
2018-04-12 11:51:47 +0100

    Changes from master

---
+++ tests/functional/pages/features/dependencies/min/min-block.page.js
@@ -0,0 +1,17 @@
+// >>> WARNING THIS PAGE WAS AUTO-GENERATED - DO NOT EDIT!!! <<<
+const QuestionPage = require('../../../surveys/question.page');
+
+class MinBlockPage extends QuestionPage {
+
+  constructor() {
+    super('min-block');
+  }
+
+  answer() {
+    return '#min-answer';
+  }
+
+  answerLabel() { return '#label-min-answer'; }
+
+}
+module.exports = new MinBlockPage();
