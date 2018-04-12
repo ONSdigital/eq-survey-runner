f849f6a217a4f67e1de5995f6d69587d1c0249dc
Mebin Abraham <mebin.abraham@qa.com>
2018-04-12 13:00:26 +0100

    Keep original metadata name

--- app/templating/metadata_context.py
+++ app/templating/metadata_context.py
@@ -10,17 +10,17 @@ def build_metadata_context(metadata):
     return {
         'eq_id': json_and_html_safe(metadata['eq_id']),
         'form_type': json_and_html_safe(metadata['form_type']),
-        'start_date': metadata.get('ref_p_start_date'),
-        'end_date': metadata.get('ref_p_end_date'),
+        'ref_p_start_date': metadata.get('ref_p_start_date'),
+        'ref_p_end_date': metadata.get('ref_p_end_date'),
         'employment_date': metadata.get('employment_date'),
         'region_code': json_and_html_safe(metadata.get('region_code')),
         'period_str': json_and_html_safe(metadata.get('period_str')),
         'return_by': metadata.get('return_by'),
         'collection_id': json_and_html_safe(metadata.get('collection_exercise_sid')),
         'tx_id': convert_tx_id(metadata['tx_id']),
-        'respondent_id': json_and_html_safe(metadata.get('ru_ref')),
+        'ru_name': json_and_html_safe(metadata.get('ru_ref')),
         'name': json_and_html_safe(metadata.get('ru_name')),
-        'trading_as': json_and_html_safe(metadata.get('trad_as'))
+        'trad_as': json_and_html_safe(metadata.get('trad_as'))
     }
 
 
