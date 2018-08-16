4a6b8ea9c8daed65289d6a15fcd62d3664074d5b
Adam Mann <manny160389@hotmail.com>
2018-08-16 15:44:37 +0100

    Update calculated summary to use answer label  - Generate page models for calculated summary

--- tests/functional/pages/features/calculated_summary/currency-total-playback.page.js
+++ tests/functional/pages/features/calculated_summary/currency-total-playback.page.js
@@ -1,7 +1,7 @@
 // >>> WARNING THIS PAGE WAS AUTO-GENERATED - DO NOT EDIT!!! <<<
-const QuestionPage = require('../../surveys/question.page');
+const CalculatedSummaryPage = require('../../calculated-summary.page');
 
-class CurrencyTotalPlaybackPage extends QuestionPage {
+class CurrencyTotalPlaybackPage extends CalculatedSummaryPage {
 
   constructor() {
     super('currency-total-playback');
@@ -11,53 +11,37 @@ class CurrencyTotalPlaybackPage extends QuestionPage {
 
   firstNumberAnswerEdit() { return '[data-qa="first-number-answer-edit"]'; }
 
+  firstNumberAnswerLabel() { return '#first-number-answer-label'; } 
+
   secondNumberAnswer() { return '#second-number-answer-answer'; }
 
   secondNumberAnswerEdit() { return '[data-qa="second-number-answer-edit"]'; }
 
-  secondNumberAnswerUnitTotal() { return '#second-number-answer-unit-total-answer'; }
-
-  secondNumberAnswerUnitTotalEdit() { return '[data-qa="second-number-answer-unit-total-edit"]'; }
+  secondNumberAnswerLabel() { return '#second-number-answer-label'; } 
 
   secondNumberAnswerAlsoInTotal() { return '#second-number-answer-also-in-total-answer'; }
 
   secondNumberAnswerAlsoInTotalEdit() { return '[data-qa="second-number-answer-also-in-total-edit"]'; }
 
+  secondNumberAnswerAlsoInTotalLabel() { return '#second-number-answer-also-in-total-label'; } 
+
   thirdNumberAnswer() { return '#third-number-answer-answer'; }
 
   thirdNumberAnswerEdit() { return '[data-qa="third-number-answer-edit"]'; }
 
-  thirdNumberAnswerUnitTotal() { return '#third-number-answer-unit-total-answer'; }
-
-  thirdNumberAnswerUnitTotalEdit() { return '[data-qa="third-number-answer-unit-total-edit"]'; }
+  thirdNumberAnswerLabel() { return '#third-number-answer-label'; } 
 
   fourthNumberAnswer() { return '#fourth-number-answer-answer'; }
 
   fourthNumberAnswerEdit() { return '[data-qa="fourth-number-answer-edit"]'; }
 
+  fourthNumberAnswerLabel() { return '#fourth-number-answer-label'; } 
+
   fourthNumberAnswerAlsoInTotal() { return '#fourth-number-answer-also-in-total-answer'; }
 
   fourthNumberAnswerAlsoInTotalEdit() { return '[data-qa="fourth-number-answer-also-in-total-edit"]'; }
 
-  fifthNumberAnswer() { return '#fifth-number-answer-answer'; }
-
-  fifthNumberAnswerEdit() { return '[data-qa="fifth-number-answer-edit"]'; }
-
-  sixthPercentAnswer() { return '#sixth-percent-answer-answer'; }
-
-  sixthPercentAnswerEdit() { return '[data-qa="sixth-percent-answer-edit"]'; }
-
-  sixthNumberAnswer() { return '#sixth-number-answer-answer'; }
-
-  sixthNumberAnswerEdit() { return '[data-qa="sixth-number-answer-edit"]'; }
-
-  calculatedSummaryTitle() { return '[data-qa="calculated-summary-title"]'; }
-
-  calculatedSummaryQuestion() { return '#calculated-summary-question'; }
-
-  calculatedSummaryAnswer() { return '#calculated-summary-answer-answer'; }
-
-  groupTitle() { return '#group'; }
+  fourthNumberAnswerAlsoInTotalLabel() { return '#fourth-number-answer-also-in-total-label'; } 
 
 }
 module.exports = new CurrencyTotalPlaybackPage();
--- tests/functional/pages/features/calculated_summary/unit-total-playback.page.js
+++ tests/functional/pages/features/calculated_summary/unit-total-playback.page.js
@@ -1,27 +1,23 @@
 // >>> WARNING THIS PAGE WAS AUTO-GENERATED - DO NOT EDIT!!! <<<
-const QuestionPage = require('../../surveys/question.page');
+const CalculatedSummaryPage = require('../../calculated-summary.page');
 
-class CurrencyTotalPlaybackPage extends QuestionPage {
+class UnitTotalPlaybackPage extends CalculatedSummaryPage {
 
   constructor() {
-    super('currency-total-playback');
+    super('unit-total-playback');
   }
 
   secondNumberAnswerUnitTotal() { return '#second-number-answer-unit-total-answer'; }
 
   secondNumberAnswerUnitTotalEdit() { return '[data-qa="second-number-answer-unit-total-edit"]'; }
 
+  secondNumberAnswerUnitTotalLabel() { return '#second-number-answer-unit-total-label'; } 
+
   thirdNumberAnswerUnitTotal() { return '#third-number-answer-unit-total-answer'; }
 
   thirdNumberAnswerUnitTotalEdit() { return '[data-qa="third-number-answer-unit-total-edit"]'; }
 
-  calculatedSummaryTitle() { return '[data-qa="calculated-summary-title"]'; }
-
-  calculatedSummaryQuestion() { return '#calculated-summary-question'; }
-
-  calculatedSummaryAnswer() { return '#calculated-summary-answer-answer'; }
-
-  groupTitle() { return '#group'; }
+  thirdNumberAnswerUnitTotalLabel() { return '#third-number-answer-unit-total-label'; } 
 
 }
-module.exports = new CurrencyTotalPlaybackPage();
+module.exports = new UnitTotalPlaybackPage();
