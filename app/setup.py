b0cdc10d2b260318a6d2f0ecb17f17bbf6e9bb23
David Neale <neale.dj@gmail.com>
2018-08-23 16:57:05 +0100

    Cache template paths

--- app/setup.py
+++ app/setup.py
@@ -21,6 +21,7 @@ from google.cloud import bigtable
 from sdc.crypto.key_store import KeyStore, validate_required_keys
 from structlog import get_logger
 
+from app import flask_theme_cache
 from app import settings
 from app.authentication.authenticator import login_manager
 from app.authentication.cookie_session import SHA256SecureCookieSessionInterface
@@ -146,6 +147,8 @@ def create_app(setting_overrides=None):  # noqa: C901  pylint: disable=too-compl
     # Add theme manager
     application.config['THEME_PATHS'] = os.path.dirname(os.path.abspath(__file__))
     Themes(application, app_identifier='surveyrunner')
+
+    application.jinja_env.globals['theme'] = flask_theme_cache.get_global_theme_template(cache)
 
     @application.before_request
     def before_request():  # pylint: disable=unused-variable
