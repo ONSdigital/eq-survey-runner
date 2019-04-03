201fd12591b227bf1e26ff9bffc92ab7d9f6faf0
Ian Wootten <hi@niftydigits.com>
2019-04-03 09:08:27 +0100

    Rename function:

--- app/decorators/opencensus_decorators.py
+++ app/decorators/opencensus_decorators.py
@@ -2,11 +2,11 @@ from functools import wraps
 from opencensus.trace import execution_context
 
 
-def capture_trace(f):
-    @wraps(f)
+def capture_trace(function_to_wrap):
+    @wraps(function_to_wrap)
     def decorated_function(*args, **kwargs):
         tracer = execution_context.get_opencensus_tracer()
-        with tracer.span(name=f.__name__):
-            return f(*args, **kwargs)
+        with tracer.span(name=function_to_wrap.__name__):
+            return function_to_wrap(*args, **kwargs)
 
     return decorated_function
