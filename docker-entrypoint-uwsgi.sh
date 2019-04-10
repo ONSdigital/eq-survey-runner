1126a2e361609c8904e76e6b1c7c51bf46c909e5
Ian Wootten <hi@niftydigits.com>
2019-04-10 15:04:56 +0100

    Remove uwsgi from deps

--- docker-entrypoint-uwsgi.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-if [ -n "$SECRETS_S3_BUCKET" ]; then
-    echo "Load Secrets from S3 Bucket [$SECRETS_S3_BUCKET]"
-    aws s3 sync s3://$SECRETS_S3_BUCKET/ /secrets
-fi
-
-uwsgi --gevent $GUNICORN_WORKERS --http 0.0.0.0:5000 --buffer-size=32768 --protocol uwsgi --plugins python3 --module application:application
