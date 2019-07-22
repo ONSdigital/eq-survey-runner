ac74829b67730379e1040382206fa9ff9fccafad
Ian Wootten <hi@niftydigits.com>
2019-07-22 15:53:11 +0100

    Add relationship location comparison to when rules

--- app/questionnaire/schema_utils.py
+++ app/questionnaire/schema_utils.py
@@ -2,18 +2,34 @@ from app.questionnaire.rules import evaluate_when_rules
 
 
 def _choose_variant(
-    block, schema, metadata, answer_store, list_store, variants_key, single_key
+    block,
+    schema,
+    metadata,
+    answer_store,
+    list_store,
+    variants_key,
+    single_key,
+    current_location=None,
 ):
     if block.get(single_key):
         return block[single_key]
 
     for variant in block.get(variants_key, []):
         when_rules = variant.get('when', [])
-        if evaluate_when_rules(when_rules, schema, metadata, answer_store, list_store):
+        if evaluate_when_rules(
+            when_rules,
+            schema,
+            metadata,
+            answer_store,
+            list_store,
+            current_location=current_location,
+        ):
             return variant[single_key]
 
 
-def choose_question_to_display(block, schema, metadata, answer_store, list_store):
+def choose_question_to_display(
+    block, schema, metadata, answer_store, list_store, current_location=None
+):
     return _choose_variant(
         block,
         schema,
@@ -22,10 +38,13 @@ def choose_question_to_display(block, schema, metadata, answer_store, list_store
         list_store,
         variants_key='question_variants',
         single_key='question',
+        current_location=current_location,
     )
 
 
-def choose_content_to_display(block, schema, metadata, answer_store, list_store):
+def choose_content_to_display(
+    block, schema, metadata, answer_store, list_store, current_location=None
+):
     return _choose_variant(
         block,
         schema,
@@ -34,15 +53,18 @@ def choose_content_to_display(block, schema, metadata, answer_store, list_store)
         list_store,
         variants_key='content_variants',
         single_key='content',
+        current_location=current_location,
     )
 
 
-def transform_variants(block, schema, metadata, answer_store, list_store):
+def transform_variants(
+    block, schema, metadata, answer_store, list_store, current_location=None
+):
     output_block = block.copy()
 
     if 'question_variants' in block:
         question = choose_question_to_display(
-            block, schema, metadata, answer_store, list_store
+            block, schema, metadata, answer_store, list_store, current_location
         )
         output_block.pop('question_variants', None)
         output_block.pop('question', None)
@@ -51,7 +73,7 @@ def transform_variants(block, schema, metadata, answer_store, list_store):
 
     if 'content_variants' in block:
         content = choose_content_to_display(
-            block, schema, metadata, answer_store, list_store
+            block, schema, metadata, answer_store, list_store, current_location
         )
         output_block.pop('content_variants', None)
         output_block.pop('content', None)
@@ -63,7 +85,12 @@ def transform_variants(block, schema, metadata, answer_store, list_store):
         for list_operation in list_operations:
             if block.get(list_operation):
                 output_block[list_operation] = transform_variants(
-                    block[list_operation], schema, metadata, answer_store, list_store
+                    block[list_operation],
+                    schema,
+                    metadata,
+                    answer_store,
+                    list_store,
+                    current_location,
                 )
 
     return output_block
