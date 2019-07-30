963a83ac72925fde066beff2339c0da60d1ff29f
James Gardiner <james@jgardiner.co.uk>
2019-07-30 15:16:50 +0100

    Add list collector summary to schema

--- app/views/handlers/block.py
+++ app/views/handlers/block.py
@@ -5,10 +5,8 @@ from structlog import get_logger
 from app.data_model.progress_store import CompletionStatus
 from app.questionnaire.location import InvalidLocationException
 from app.questionnaire.path_finder import PathFinder
-from app.questionnaire.placeholder_renderer import PlaceholderRenderer
 from app.questionnaire.questionnaire_store_updater import QuestionnaireStoreUpdater
 from app.questionnaire.router import Router
-from app.questionnaire.schema_utils import transform_variants
 
 logger = get_logger()
 
@@ -17,9 +15,10 @@ class BlockHandler:
     def __init__(self, schema, questionnaire_store, language, current_location):
         self._schema = schema
         self._questionnaire_store = questionnaire_store
+        self._language = language
         self._current_location = current_location
+        self.block = self._schema.get_block(current_location.block_id)
 
-        self.rendered_block = self._render_block(current_location.block_id, language)
         self._questionnaire_store_updater = None
         self._path_finder = None
         self._router = None
@@ -37,7 +36,7 @@ class BlockHandler:
                 self._current_location,
                 self._schema,
                 self._questionnaire_store,
-                self.rendered_block.get('question'),
+                self.block.get('question'),
             )
         return self._questionnaire_store_updater
 
@@ -104,24 +103,6 @@ class BlockHandler:
     def _get_routing_path(self):
         section = self._schema.get_section_for_block_id(self._current_location.block_id)
         return self.path_finder.routing_path(section)
-
-    def _render_block(self, block_id, language):
-        block_schema = self._schema.get_block(block_id)
-        transformed_block = transform_variants(
-            block_schema,
-            self._schema,
-            self._questionnaire_store.metadata,
-            self._questionnaire_store.answer_store,
-            self._questionnaire_store.list_store,
-            self._current_location,
-        )
-
-        placeholder_renderer = PlaceholderRenderer(
-            language=language,
-            answer_store=self._questionnaire_store.answer_store,
-            metadata=self._questionnaire_store.metadata,
-        )
-        return placeholder_renderer.render(transformed_block)
 
     def _update_section_completeness(self):
         if self.path_finder.is_path_complete(self._routing_path):
