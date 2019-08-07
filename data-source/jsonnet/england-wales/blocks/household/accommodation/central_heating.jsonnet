be116df6e43bb7c7abcc2bb93415504582184be4
Richard Webb <richard@rfwebb.com>
2019-08-07 19:13:57 +0100

    Build Census Household Accommodation (#2256)

---
+++ data-source/jsonnet/england-wales/blocks/household/accommodation/central_heating.jsonnet
@@ -0,0 +1,74 @@
+local placeholders = import '../../../lib/placeholders.libsonnet';
+
+{
+  type: 'Question',
+  id: 'central-heating',
+  question: {
+    id: 'central-heating-question',
+    title: {
+      text: 'What type of central heating does <em>{address}</em> have?',
+      placeholders: [placeholders.address],
+    },
+    type: 'General',
+    guidance: {
+      contents: [
+        {
+          title: 'Include central heating systems that generate heat for multiple rooms whether or not you use them',
+        },
+      ],
+    },
+    answers: [
+      {
+        id: 'central-heating-answer',
+        mandatory: true,
+        type: 'Checkbox',
+        options: [
+          {
+            label: 'No central heating',
+            value: 'No central heating',
+          },
+          {
+            label: 'Mains gas',
+            value: 'Mains gas',
+          },
+          {
+            label: 'Tank or bottled gas',
+            value: 'Tank or bottled gas',
+          },
+          {
+            label: 'Electric',
+            value: 'Electric',
+            description: 'Including storage heaters',
+          },
+          {
+            label: 'Oil',
+            value: 'Oil',
+          },
+          {
+            label: 'Wood',
+            value: 'Wood',
+            description: 'For example, logs, waste wood or pellets',
+          },
+          {
+            label: 'Solid fuel',
+            value: 'Solid fuel',
+            description: 'For example, coal',
+          },
+          {
+            label: 'Renewable energy',
+            value: 'Renewable energy',
+            description: 'For example, solar thermal or heat pumps',
+          },
+          {
+            label: 'District or communal heat networks',
+            value: 'District or communal heat networks',
+          },
+          {
+            label: 'Other',
+            value: 'Other',
+          },
+        ],
+      },
+    ],
+  },
+}
--- scripts/build_schemas.sh
+++ scripts/build_schemas.sh
@@ -2,37 +2,37 @@
 
 set -e
 
+mkdir -p data/en
+
 # Build schema for each region
 for region_code in GB-WLS GB-ENG GB-NIR; do
     # Lowercase the region code and replace '-' with '_'
     FORMATTED_REGION_CODE=$(echo "${region_code}" | tr '[:upper:]' '[:lower:]' | tr - _)
-    DESTINATION_FILE="data-source/jsonnet/census_individual_${FORMATTED_REGION_CODE}.json"
 
     CENSUS_DATE="2019-10-13"
     CENSUS_MONTH_YEAR_DATE="2019-10"
 
-    if [[ "$region_code" = "GB-NIR" ]]; then
-        SOURCE_FILE="data-source/jsonnet/northern-ireland/census_individual.jsonnet"
+    for census_type in "individual" "household"; do
 
-        jsonnet --tla-str region_code="${region_code}" --tla-str census_date="${CENSUS_DATE}" "${SOURCE_FILE}" > "${DESTINATION_FILE}"
-    else
-        SOURCE_FILE="data-source/jsonnet/england-wales/census_individual.jsonnet"
+        DESTINATION_FILE="data/en/census_${census_type}_${FORMATTED_REGION_CODE}.json"
 
-        jsonnet --tla-str region_code="${region_code}" --tla-str census_date="${CENSUS_DATE}" --tla-str census_month_year_date="${CENSUS_MONTH_YEAR_DATE}" "${SOURCE_FILE}" > "${DESTINATION_FILE}"
-    fi
+        if [[ "$region_code" = "GB-NIR" ]]; then
+            SOURCE_FILE="data-source/jsonnet/northern-ireland/census_${census_type}.jsonnet"
 
-    echo "Built ${DESTINATION_FILE}"
+            if [ -f "$SOURCE_FILE" ]; then
+                jsonnet --tla-str region_code="${region_code}" --tla-str census_date="${CENSUS_DATE}" "${SOURCE_FILE}" > "${DESTINATION_FILE}"
+            else
+                echo "Ignoring $SOURCE_FILE as it does not exist"
+            fi
+        else
+            SOURCE_FILE="data-source/jsonnet/england-wales/census_${census_type}.jsonnet"
+
+            jsonnet --tla-str region_code="${region_code}" --tla-str census_date="${CENSUS_DATE}" --tla-str census_month_year_date="${CENSUS_MONTH_YEAR_DATE}" "${SOURCE_FILE}" > "${DESTINATION_FILE}"
+        fi
+
+        echo "Built ${DESTINATION_FILE}"
+    done
 done
 
 # Move newly built schemas to 'en' dir
-mkdir -p data/en
-mv data-source/jsonnet/*.json data/en
 cp data-source/json/*.json data/en
-
-echo "Creating temporary household schemas based on individual schema in 'data' dir"
-
-# Until household is delivered, temporarily create household schema as copies of the individual schema
-cp data/en/census_individual_gb_wls.json data/en/census_household_gb_wls.json
-cp data/en/census_individual_gb_eng.json data/en/census_household_gb_eng.json
-cp data/en/census_individual_gb_nir.json data/en/census_household_gb_nir.json
-
