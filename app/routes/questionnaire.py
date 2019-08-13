2b964c95d70777815be725b33788eaefbf083368
Ian Wootten <hi@niftydigits.com>
2019-08-13 11:59:55 +0100

    Rename variable

--- app/routes/questionnaire.py
+++ app/routes/questionnaire.py
@@ -111,12 +111,12 @@ def get_questionnaire(schema, questionnaire_store):
         list_store=questionnaire_store.list_store,
     )
     completed_section_ids = questionnaire_store.progress_store.completed_section_ids
-    hub_required_sections_complete = all(
+    are_hub_required_sections_complete = all(
         section_id in completed_section_ids
         for section_id in schema.get_section_ids_required_for_hub()
     )
 
-    if not schema.is_hub_enabled() or not hub_required_sections_complete:
+    if not schema.is_hub_enabled() or not are_hub_required_sections_complete:
         redirect_location = router.get_first_incomplete_location_in_survey()
         return redirect(redirect_location.url())
 
