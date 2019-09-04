24c737daa941cf0914178b7ab3b2a8ee5e18554b
Richard McCarthy <richard.mccarthy.ons@gmail.com>
2019-09-04 14:53:48 +0100

    pr comments

--- tests/functional/spec/components/checkbox/checkbox_numeric_detail_answers.spec.js
+++ tests/functional/spec/components/checkbox/checkbox_numeric_detail_answers.spec.js
@@ -5,112 +5,81 @@ const SummaryPage = require('../../../generated_pages/checkbox_numeric_detail_an
 
 describe('Checkbox with a numeric "detail_answer" option', function() {
 
-  const checkbox_schema = 'test_checkbox_numeric_detail_answers.json';
+  beforeEach(function() {
+    return helpers.openQuestionnaire('test_checkbox_numeric_detail_answers.json').then(() => {
+      return browser.click(CheckboxNumericDetailPage.other());
+    });
+  });
 
   it('Given a numeric detail answer options are available, When the user clicks an option, Then the detail answer input should be visible.', function() {
-    // Given
-    return helpers.openQuestionnaire(checkbox_schema).then(() => {
-      return browser
-      // When
-        .click(CheckboxNumericDetailPage.other())
-      // Then
-        .isVisible(CheckboxNumericDetailPage.otherDetail()).should.eventually.be.true;
-    });
+      return browser.isVisible(CheckboxNumericDetailPage.otherDetail()).should.eventually.be.true;
   });
 
   it('Given a numeric detail answer, When the user does not provide any text, Then just the option value should be displayed on the summary screen', function() {
-    // Given
-    return helpers.openQuestionnaire(checkbox_schema).then(() => {
-      return browser
-      // When
-        .click(CheckboxNumericDetailPage.other())
-        .isVisible(CheckboxNumericDetailPage.otherDetail()).should.eventually.be.true
-        .click(CheckboxNumericDetailPage.submit())
-      // Then
-        .getText(SummaryPage.checkboxNumericDetailAnswer()).should.eventually.contain('Other');
-    });
+    return browser
+    // When
+      .isVisible(CheckboxNumericDetailPage.otherDetail()).should.eventually.be.true
+      .click(CheckboxNumericDetailPage.submit())
+    // Then
+      .getText(SummaryPage.checkboxNumericDetailAnswer()).should.eventually.contain('Other');
   });
 
   it('Given a numeric detail answer, When the user provides text, Then that text should be displayed on the summary screen', function() {
-    // Given
-    return helpers.openQuestionnaire(checkbox_schema).then(() => {
-      return browser
-      // When
-        .click(CheckboxNumericDetailPage.other())
-        .setValue(CheckboxNumericDetailPage.otherDetail(), '15')
-        .click(CheckboxNumericDetailPage.submit())
-      // Then
-        .getText(SummaryPage.checkboxNumericDetailAnswer()).should.eventually.contain('15');
-    });
+    return browser
+    // When
+      .setValue(CheckboxNumericDetailPage.otherDetail(), '15')
+      .click(CheckboxNumericDetailPage.submit())
+    // Then
+      .getText(SummaryPage.checkboxNumericDetailAnswer()).should.eventually.contain('15');
   });
 
   it('Given a numeric detail answer, When the user provides text, An error should be displayed', function() {
-    // Given
-    return helpers.openQuestionnaire(checkbox_schema).then(() => {
-      return browser
-      // When
-        .click(CheckboxNumericDetailPage.other())
-        .setValue(CheckboxNumericDetailPage.otherDetail(), 'fhdjkshfjkds')
-        .click(CheckboxNumericDetailPage.submit())
-      // Then
-        .isVisible(CheckboxNumericDetailPage.error()).should.eventually.be.true
-        .getText(CheckboxNumericDetailPage.errorNumber(1)).should.eventually.contain('Please enter an integer');
-    });
+    return browser
+    // When
+      .setValue(CheckboxNumericDetailPage.otherDetail(), 'fhdjkshfjkds')
+      .click(CheckboxNumericDetailPage.submit())
+    // Then
+      .isVisible(CheckboxNumericDetailPage.error()).should.eventually.be.true
+      .getText(CheckboxNumericDetailPage.errorNumber(1)).should.eventually.contain('Please enter an integer');
   });
 
   it('Given a numeric detail answer, When the user provides a number larger than 20, An error should be displayed', function() {
-    // Given
-    return helpers.openQuestionnaire(checkbox_schema).then(() => {
-      return browser
-      // When
-        .click(CheckboxNumericDetailPage.other())
-        .setValue(CheckboxNumericDetailPage.otherDetail(), '250')
-        .click(CheckboxNumericDetailPage.submit())
-      // Then
-        .isVisible(CheckboxNumericDetailPage.error()).should.eventually.be.true
-        .getText(CheckboxNumericDetailPage.errorNumber(1)).should.eventually.contain('Number is too large');
-    });
+    return browser
+    // When
+      .setValue(CheckboxNumericDetailPage.otherDetail(), '250')
+      .click(CheckboxNumericDetailPage.submit())
+    // Then
+      .isVisible(CheckboxNumericDetailPage.error()).should.eventually.be.true
+      .getText(CheckboxNumericDetailPage.errorNumber(1)).should.eventually.contain('Number is too large');
   });
 
   it('Given a numeric detail answer, When the user provides a number less than 0, An error should be displayed', function() {
-    // Given
-    return helpers.openQuestionnaire(checkbox_schema).then(() => {
-      return browser
-      // When
-        .click(CheckboxNumericDetailPage.other())
-        .setValue(CheckboxNumericDetailPage.otherDetail(), '-1')
-        .click(CheckboxNumericDetailPage.submit())
-      // Then
-        .isVisible(CheckboxNumericDetailPage.error()).should.eventually.be.true
-        .getText(CheckboxNumericDetailPage.errorNumber(1)).should.eventually.contain('Number cannot be less than zero');
-    });
+    return browser
+    // When
+      .setValue(CheckboxNumericDetailPage.otherDetail(), '-1')
+      .click(CheckboxNumericDetailPage.submit())
+    // Then
+      .isVisible(CheckboxNumericDetailPage.error()).should.eventually.be.true
+      .getText(CheckboxNumericDetailPage.errorNumber(1)).should.eventually.contain('Number cannot be less than zero');
   });
 
   it('Given a numeric detail answer, When the user provides text, An error should be displayed and the text in the textbox should be kept', function() {
-    // Given
-    return helpers.openQuestionnaire(checkbox_schema).then(() => {
-      return browser
-      // When
-        .click(CheckboxNumericDetailPage.other())
-        .setValue(CheckboxNumericDetailPage.otherDetail(), 'biscuits')
-        .click(CheckboxNumericDetailPage.submit())
-      // Then
-        .isVisible(CheckboxNumericDetailPage.error()).should.eventually.be.true
-        .getText(CheckboxNumericDetailPage.errorNumber(1)).should.eventually.contain('Please enter an integer').pause(1000)
-        .getValue(CheckboxNumericDetailPage.otherDetail()).should.eventually.contain('biscuits');
-    });
+    return browser
+    // When
+      .setValue(CheckboxNumericDetailPage.otherDetail(), 'biscuits')
+      .click(CheckboxNumericDetailPage.submit())
+    // Then
+      .isVisible(CheckboxNumericDetailPage.error()).should.eventually.be.true
+      .getText(CheckboxNumericDetailPage.errorNumber(1)).should.eventually.contain('Please enter an integer').pause(1000)
+      .getValue(CheckboxNumericDetailPage.otherDetail()).should.eventually.contain('biscuits');
   });
 
   it('Given a numeric detail answer, When the user enters "0" and submits, Then "0" should be displayed on the summary screen', function() {
-    // Given
-    return helpers.openQuestionnaire(checkbox_schema).then(() => {
-      return browser
-      // When
-        .click(CheckboxNumericDetailPage.other())
-        .setValue(CheckboxNumericDetailPage.otherDetail(), '0')
-        .click(CheckboxNumericDetailPage.submit())
-      // Then
-        .getText(SummaryPage.checkboxNumericDetailAnswer()).should.eventually.contain('0');
-    });
+    return browser
+    // When
+      .setValue(CheckboxNumericDetailPage.otherDetail(), '0')
+      .click(CheckboxNumericDetailPage.submit())
+    // Then
+      .getText(SummaryPage.checkboxNumericDetailAnswer()).should.eventually.contain('0');
   });
 });
--- tests/functional/spec/components/radio/radio_numeric_detail_answers.spec.js
+++ tests/functional/spec/components/radio/radio_numeric_detail_answers.spec.js
@@ -5,112 +5,81 @@ const SummaryPage = require('../../../generated_pages/radio_numeric_detail_answe
 
 describe('Radio with a numeric "detail_answer" option', function() {
 
-  const radio_schema = 'test_radio_numeric_detail_answers.json';
-
-  it('Given a numeric detail answer options are available, When the user clicks an option, Then the detail answer input should be visible.', function() {
-    // Given
-    return helpers.openQuestionnaire(radio_schema).then(() => {
-      return browser
-      // When
-        .click(RadioNumericDetailPage.other())
-      // Then
-        .isVisible(RadioNumericDetailPage.otherDetail()).should.eventually.be.true;
+  beforeEach(function() {
+    return helpers.openQuestionnaire('test_radio_numeric_detail_answers.json').then(() => {
+      return browser.click(RadioNumericDetailPage.other());
     });
   });
 
+  it('Given a numeric detail answer options are available, When the user clicks an option, Then the detail answer input should be visible.', function() {
+      return browser.isVisible(RadioNumericDetailPage.otherDetail()).should.eventually.be.true;
+  });
+
   it('Given a numeric detail answer, When the user does not provide any text, Then just the option value should be displayed on the summary screen', function() {
-    // Given
-    return helpers.openQuestionnaire(radio_schema).then(() => {
       return browser
       // When
-        .click(RadioNumericDetailPage.other())
         .isVisible(RadioNumericDetailPage.otherDetail()).should.eventually.be.true
         .click(RadioNumericDetailPage.submit())
       // Then
         .getText(SummaryPage.radioAnswerNumericDetail()).should.eventually.contain('Other');
-    });
   });
 
   it('Given a numeric detail answer, When the user provides text, Then that text should be displayed on the summary screen', function() {
-    // Given
-    return helpers.openQuestionnaire(radio_schema).then(() => {
       return browser
       // When
-        .click(RadioNumericDetailPage.other())
         .setValue(RadioNumericDetailPage.otherDetail(), '15')
         .click(RadioNumericDetailPage.submit())
       // Then
         .getText(SummaryPage.radioAnswerNumericDetail()).should.eventually.contain('15');
-    });
   });
 
   it('Given a numeric detail answer, When the user provides text, An error should be displayed', function() {
-    // Given
-    return helpers.openQuestionnaire(radio_schema).then(() => {
       return browser
       // When
-        .click(RadioNumericDetailPage.other())
         .setValue(RadioNumericDetailPage.otherDetail(), 'fhdjkshfjkds')
         .click(RadioNumericDetailPage.submit())
       // Then
         .isVisible(RadioNumericDetailPage.error()).should.eventually.be.true
         .getText(RadioNumericDetailPage.errorNumber(1)).should.eventually.contain('Please enter an integer');
-    });
   });
 
   it('Given a numeric detail answer, When the user provides a number larger than 20, An error should be displayed', function() {
-    // Given
-    return helpers.openQuestionnaire(radio_schema).then(() => {
       return browser
       // When
-        .click(RadioNumericDetailPage.other())
         .setValue(RadioNumericDetailPage.otherDetail(), '250')
         .click(RadioNumericDetailPage.submit())
       // Then
         .isVisible(RadioNumericDetailPage.error()).should.eventually.be.true
         .getText(RadioNumericDetailPage.errorNumber(1)).should.eventually.contain('Number is too large');
-    });
   });
 
   it('Given a numeric detail answer, When the user provides a number less than 0, An error should be displayed', function() {
-    // Given
-    return helpers.openQuestionnaire(radio_schema).then(() => {
       return browser
       // When
-        .click(RadioNumericDetailPage.other())
         .setValue(RadioNumericDetailPage.otherDetail(), '-1')
         .click(RadioNumericDetailPage.submit())
       // Then
         .isVisible(RadioNumericDetailPage.error()).should.eventually.be.true
         .getText(RadioNumericDetailPage.errorNumber(1)).should.eventually.contain('Number cannot be less than zero');
-    });
   });
 
   it('Given a numeric detail answer, When the user provides text, An error should be displayed and the text in the textbox should be kept', function() {
-    // Given
-    return helpers.openQuestionnaire(radio_schema).then(() => {
       return browser
       // When
-        .click(RadioNumericDetailPage.other())
         .setValue(RadioNumericDetailPage.otherDetail(), 'biscuits')
         .click(RadioNumericDetailPage.submit())
       // Then
         .isVisible(RadioNumericDetailPage.error()).should.eventually.be.true
         .getText(RadioNumericDetailPage.errorNumber(1)).should.eventually.contain('Please enter an integer')
         .getValue(RadioNumericDetailPage.otherDetail()).should.eventually.contain('biscuits');
-    });
   });
 
   it('Given a numeric detail answer, When the user enters "0" and submits, Then "0" should be displayed on the summary screen', function() {
-    // Given
-    return helpers.openQuestionnaire(radio_schema).then(() => {
       return browser
       // When
-        .click(RadioNumericDetailPage.other())
         .setValue(RadioNumericDetailPage.otherDetail(), '0')
         .click(RadioNumericDetailPage.submit())
       // Then
         .getText(SummaryPage.radioAnswerNumericDetail()).should.eventually.contain('0');
-    });
   });
 });
