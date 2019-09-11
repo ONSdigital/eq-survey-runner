5f6bdd4e8d3f9f1e1e94260386f1a3549c57963a
Rhys Berrow <rhys.berrow@ons.gov.uk>
2019-09-11 13:05:47 +0100

    Added the display_address claim to the payload

--- app/submitter/converter.py
+++ app/submitter/converter.py
@@ -88,6 +88,8 @@ def convert_answers(schema, questionnaire_store, routing_path, flushed=False):
         payload['case_id'] = metadata['case_id']
     if metadata.get('case_ref'):
         payload['case_ref'] = metadata['case_ref']
+    if metadata.get('display_address'):
+        payload['display_address'] = metadata['display_address']
 
     if schema.json['data_version'] == '0.0.3':
         payload['data'] = {
