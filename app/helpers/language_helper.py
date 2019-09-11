ae135ba6019f193689816eec358e631bb240df93
Andrew Maddaford <ajmaddaford@gmail.com>
2019-09-11 15:05:24 +0100

    Add note about use of en_US

--- app/helpers/language_helper.py
+++ app/helpers/language_helper.py
@@ -6,6 +6,9 @@ from app.globals import get_metadata, get_session_store
 from app.questionnaire.questionnaire_schema import DEFAULT_LANGUAGE_CODE
 from app.utilities.schema import get_allowed_languages
 
+# en_US has been used for Ulstér Scotch as there is no language code in the
+# CLDR (http://cldr.unicode.org) for it and this is required for survey runner
+# to work (http://babel.pocoo.org/en/latest/locale.html).
 
 LANGUAGE_TEXT = {
     'en': 'English',
