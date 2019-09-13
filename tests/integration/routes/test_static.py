5f6d4d10c3d42a979774ae63d05d6f8984fa5250
Richard McCarthy <richard.mccarthy.ons@gmail.com>
2019-09-13 11:41:54 +0100

    updated formatting

--- tests/integration/routes/test_static.py
+++ tests/integration/routes/test_static.py
@@ -2,7 +2,6 @@ from tests.integration.integration_test_case import IntegrationTestCase
 
 
 class TestStatic(IntegrationTestCase):
-
     def test_privacy(self):
         self.get('/privacy')
         self.assertInBody('Who can access the information?')
