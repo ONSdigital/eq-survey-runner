699287bdeec3a329277fc8795ecbb01578e538b8
Rhys Berrow <47635349+berroar@users.noreply.github.com>
2019-09-16 10:21:41 +0100

    Adding Ulster Scots translations for NI Household, Individual and static content (#2371)

--- scripts/translate_schemas.py
+++ scripts/translate_schemas.py
@@ -13,6 +13,7 @@ logger.addHandler(logging.StreamHandler(sys.stdout))
 
 TRANSLATION_MAP = {
     'cy': ['test_language', 'census_individual_gb_wls', 'census_household_gb_wls'],
+    'eo': ['census_individual_gb_nir', 'census_household_gb_nir'],
     'ga': ['census_individual_gb_nir', 'census_household_gb_nir'],
 }
 
