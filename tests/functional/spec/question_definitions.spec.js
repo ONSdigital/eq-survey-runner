af6b6d28988fee0f0b200b2fbd2cbc93e4a5ac75
Richard McCarthy <richard.mccarthy.ons@gmail.com>
2019-09-18 09:38:17 +0100

    fix tests

--- tests/functional/spec/question_definitions.spec.js
+++ tests/functional/spec/question_definitions.spec.js
@@ -11,58 +11,35 @@ describe('Component: Definition', function() {
     it('When I click the title link, then the description and "Hide this" button should be visible', function() {
 
       return browser
-        .isVisible(DefinitionPage.definitionContent1()).should.eventually.be.false
-        .isVisible(DefinitionPage.definitionButton1()).should.eventually.be.false
+        .isVisible(DefinitionPage.definitionContent2()).should.eventually.be.false
+        .isVisible(DefinitionPage.definitionButton2()).should.eventually.be.false
 
         // When
-        .click(DefinitionPage.definitionTitle1())
+        .click(DefinitionPage.definitionTitle2())
 
-        // Then
-        .waitForVisible(DefinitionPage.definitionContent1(), 300)
-        .waitForVisible(DefinitionPage.definitionButton1(), 300);
+        // // Then
+        .waitForVisible(DefinitionPage.definitionContent2(), 300)
+        .waitForVisible(DefinitionPage.definitionButton2(), 300);
 
     });
 
     it('When I click the title link twice, then the description and "Hide this" button should not be visible', function() {
 
       return browser
-        .isVisible(DefinitionPage.definitionContent1()).should.eventually.be.false
-        .isVisible(DefinitionPage.definitionButton1()).should.eventually.be.false
+        .isVisible(DefinitionPage.definitionContent2()).should.eventually.be.false
+        .isVisible(DefinitionPage.definitionButton2()).should.eventually.be.false
 
         // When
-        .click(DefinitionPage.definitionTitle1())
-        .click(DefinitionPage.definitionTitle1())
+        .click(DefinitionPage.definitionTitle2())
+        .click(DefinitionPage.definitionTitle2())
 
         // Then
-        .waitForVisible(DefinitionPage.definitionContent1(), 300, true)
-        .waitForVisible(DefinitionPage.definitionButton1(), 300, true);
+        .waitForVisible(DefinitionPage.definitionContent2(), 300, true)
+        .waitForVisible(DefinitionPage.definitionButton2(), 300, true);
 
     });
 
     it('When I click the title link then click "Hide this" button, then the description and button should not be visible', function() {
-
-      return browser
-        .isVisible(DefinitionPage.definitionContent1()).should.eventually.be.false
-        .isVisible(DefinitionPage.definitionButton1()).should.eventually.be.false
-
-        // When
-        .click(DefinitionPage.definitionTitle1())
-
-        // Then
-        .waitForVisible(DefinitionPage.definitionContent1(), 300)
-        .waitForVisible(DefinitionPage.definitionButton1(), 300)
-
-        // When
-        .click(DefinitionPage.definitionButton1())
-
-        // Then
-        .waitForVisible(DefinitionPage.definitionContent1(), 300, true)
-        .waitForVisible(DefinitionPage.definitionButton1(), 300, true);
-
-    });
-
-
-    it('When I click the second definition\'s title link then the description and "Hide this" button for the second definition should be visible', function() {
 
       return browser
         .isVisible(DefinitionPage.definitionContent2()).should.eventually.be.false
@@ -73,7 +50,30 @@ describe('Component: Definition', function() {
 
         // Then
         .waitForVisible(DefinitionPage.definitionContent2(), 300)
-        .waitForVisible(DefinitionPage.definitionButton2(), 300);
+        .waitForVisible(DefinitionPage.definitionButton2(), 300)
+
+        // When
+        .click(DefinitionPage.definitionButton2())
+
+        // Then
+        .waitForVisible(DefinitionPage.definitionContent2(), 300, true)
+        .waitForVisible(DefinitionPage.definitionButton2(), 300, true);
+
+    });
+
+
+    it('When I click the second definition\'s title link then the description and "Hide this" button for the second definition should be visible', function() {
+
+      return browser
+        .isVisible(DefinitionPage.definitionContent3()).should.eventually.be.false
+        .isVisible(DefinitionPage.definitionButton3()).should.eventually.be.false
+
+        // When
+        .click(DefinitionPage.definitionTitle3())
+
+        // Then
+        .waitForVisible(DefinitionPage.definitionContent3(), 300)
+        .waitForVisible(DefinitionPage.definitionButton3(), 300);
 
     });
 
