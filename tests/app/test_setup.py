7a8e7d007e677bab29ca433ddedf40e45d3e6b3e
James Gardiner <james@jgardiner.co.uk>
2019-10-10 14:07:20 +0100

    Remove AttrDict

--- tests/app/test_setup.py
+++ tests/app/test_setup.py
@@ -1,31 +1,21 @@
 from app.helpers import get_span_and_trace
 
 
-class AttrDict(dict):
-    def __init__(self, *args, **kwargs):
-        super(AttrDict, self).__init__(*args, **kwargs)
-        self.__dict__ = self
-
-
 def cloud_trace_header():
-    header = AttrDict()
-    header.update(
-        {'headers': {'X-Cloud-Trace-Context': '0123456789/0123456789012345678901;o=1'}}
-    )
-    return header
+    return {'X-Cloud-Trace-Context': '0123456789/0123456789012345678901;o=1'}
 
 
 def test_get_span_and_trace():
     header = cloud_trace_header()
-    span, trace = get_span_and_trace(header.headers)  # pylint: disable=no-member
+    span, trace = get_span_and_trace(header)  # pylint: disable=no-member
     assert trace == '0123456789'
     assert span == '0123456789012345678901'
 
 
 def test_get_span_and_trace_no_xcloud_header():
     header = cloud_trace_header()
-    del header.headers['X-Cloud-Trace-Context']  # pylint: disable=no-member
-    span, trace = get_span_and_trace(header.headers)  # pylint: disable=no-member
+    del header['X-Cloud-Trace-Context']  # pylint: disable=no-member
+    span, trace = get_span_and_trace(header)  # pylint: disable=no-member
 
     assert trace is None
     assert span is None
@@ -33,9 +23,7 @@ def test_get_span_and_trace_no_xcloud_header():
 
 def test_get_span_and_trace_malformed_xcloud_header():  # pylint: disable=no-member
     header = cloud_trace_header()
-    header.headers[  # pylint: disable=no-member
-        'X-Cloud-Trace-Context'
-    ] = 'not a real trace context'
-    span, trace = get_span_and_trace(header.headers)  # pylint: disable=no-member
+    header['X-Cloud-Trace-Context'] = 'not a real trace context'
+    span, trace = get_span_and_trace(header)  # pylint: disable=no-member
     assert trace is None
     assert span is None
