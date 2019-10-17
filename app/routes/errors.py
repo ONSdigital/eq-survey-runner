c9df8eb2ecf24a896e4242e6781a2fc6d55203a2
James Gardiner <james@jgardiner.co.uk>
2019-10-17 11:06:39 +0100

    Update log formatting and stream handlers (#2432)

--- app/routes/errors.py
+++ app/routes/errors.py
@@ -15,49 +15,14 @@ logger = get_logger()
 errors_blueprint = Blueprint('errors', __name__)
 
 
-@errors_blueprint.app_errorhandler(401)
-@errors_blueprint.app_errorhandler(CSRFError)
-@errors_blueprint.app_errorhandler(NoTokenException)
-def unauthorized(error=None):
-    log_exception(error, 401)
-    return _render_error_page(401, 'session-expired')
-
-
-@errors_blueprint.app_errorhandler(InvalidTokenException)
-def forbidden(error=None):
-    log_exception(error, 403)
-    return _render_error_page(403)
-
-
-@errors_blueprint.app_errorhandler(SubmissionFailedException)
-@errors_blueprint.app_errorhandler(Exception)
-def internal_server_error(error=None):
-    try:
-        log_exception(error, 500)
-        return _render_error_page(500)
-    except Exception:  # pylint:disable=broad-except
-        logger.error(
-            'an error has occurred when rendering 500 error',
-            exc_info=True,
-            url=request.url,
-            status_code=500,
-        )
-        return render_template(template='errors/500'), 500
-
-
-@errors_blueprint.app_errorhandler(403)
-@errors_blueprint.app_errorhandler(404)
-def http_exception(error):
-    log_exception(error, error.code)
-    return _render_error_page(error.code)
-
-
-def log_exception(error, status_code):
+def log_error(error, status_code):
     metadata = get_metadata(current_user)
     if metadata:
         logger.bind(tx_id=metadata['tx_id'])
 
-    logger.error(
+    log = logger.warning if status_code < 500 else logger.error
+
+    log(
         'an error has occurred',
         exc_info=error,
         url=request.url,
@@ -74,3 +39,40 @@ def _render_error_page(status_code, template=None):
         render_template(template=f'errors/{template}', using_edge=using_edge),
         status_code,
     )
+
+
+@errors_blueprint.app_errorhandler(401)
+@errors_blueprint.app_errorhandler(CSRFError)
+@errors_blueprint.app_errorhandler(NoTokenException)
+def unauthorized(error=None):
+    log_error(error, 401)
+    return _render_error_page(401, 'session-expired')
+
+
+@errors_blueprint.app_errorhandler(InvalidTokenException)
+def forbidden(error=None):
+    log_error(error, 403)
+    return _render_error_page(403)
+
+
+@errors_blueprint.app_errorhandler(SubmissionFailedException)
+@errors_blueprint.app_errorhandler(Exception)
+def internal_server_error(error=None):
+    try:
+        log_error(error, 500)
+        return _render_error_page(500)
+    except Exception:  # pylint:disable=broad-except
+        logger.error(
+            'an error has occurred when rendering 500 error',
+            exc_info=True,
+            url=request.url,
+            status_code=500,
+        )
+        return render_template(template='errors/500'), 500
+
+
+@errors_blueprint.app_errorhandler(403)
+@errors_blueprint.app_errorhandler(404)
+def http_exception(error):
+    log_error(error, error.code)
+    return _render_error_page(error.code)
