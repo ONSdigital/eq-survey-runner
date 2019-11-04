9c054cce715a0982fe63465f3cf161a38289b02e
Ian Wootten <hi@niftydigits.com>
2019-11-04 10:41:22 +0000

    Use the number of available cores

--- tests/functional/wdio.conf.js
+++ tests/functional/wdio.conf.js
@@ -1,3 +1,5 @@
+const os = require('os');
+
 exports.config = {
     //
     // ====================
@@ -42,17 +44,13 @@ exports.config = {
     // and 30 processes will get spawned. The property handles how many capabilities
     // from the same test should run tests.
     //
-    maxInstances: 5,
+    maxInstances: os.cpus().length,
     //
     // If you have trouble getting all important capabilities together, check out the
     // Sauce Labs platform configurator - a great tool to configure your capabilities:
     // https://docs.saucelabs.com/reference/platforms-configurator
     //
     capabilities: [{
-        // maxInstances can get overwritten per capability. So if you have an in-house Selenium
-        // grid with only 5 firefox instances available you can make sure that not more than
-        // 5 instances get started at a time.
-        maxInstances: 5,
         //
         browserName: 'chrome',
         // If outputDir is provided WebdriverIO can capture driver session logs
