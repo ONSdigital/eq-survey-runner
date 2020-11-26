80731159d65af6f5ad941e36e4a3b6b21c59e9a7
Thomas Baggaley <tbaggaley@pm.me>
2020-11-26 14:11:28 +0000

    ✅ Add test for analytics_allowed function

--- app/utilities/cookies.py
+++ app/utilities/cookies.py
@@ -5,6 +5,6 @@ def analytics_allowed(request):
     cookie_policy = request.cookies.get('ons_cookie_policy')
 
     if cookie_policy:
-        return json.loads(cookie_policy.replace('\'', '\"'))['usage']
+        return json.loads(cookie_policy.replace("'", '"'))['usage']
 
     return False
