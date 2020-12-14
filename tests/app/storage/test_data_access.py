f3537c12751a82d82b3bc7c43aeec04773b7779a
Matthew Elcock <mattiose@outlook.com>
2020-12-14 16:05:51 +0000

    Refactoring

--- tests/app/storage/test_data_access.py
+++ tests/app/storage/test_data_access.py
@@ -12,6 +12,11 @@ from tests.app.app_context_test_case import AppContextTestCase
 USER_ID = 'someuser'
 STATE_DATA = 'statedata'
 VERSION = 1
+COLLECTION_EXERCISE_ID = 'someid'
+FORM_TYPE = 'someformtype'
+RU_REF = 'someref'
+EQ_ID = 'someid'
+
 
 
 class TestDataAccess(AppContextTestCase):
@@ -30,7 +35,7 @@ class TestDataAccess(AppContextTestCase):
         self.assertTrue(getattr(model, '_use_dynamo'))
 
     def test_get_by_key_rds_fallback(self):
-        rds_model = models.QuestionnaireState(USER_ID, STATE_DATA, VERSION)
+        rds_model = models.QuestionnaireState(USER_ID, STATE_DATA, VERSION, COLLECTION_EXERCISE_ID, FORM_TYPE, RU_REF, EQ_ID)
 
         with mock.patch('app.storage.dynamo_api.get_item', return_value=None), \
              mock.patch.object(models.QuestionnaireState, 'query') as query:
@@ -50,7 +55,7 @@ class TestDataAccess(AppContextTestCase):
         self.assertIsNone(model)
 
     def test_put(self):
-        model = QuestionnaireState(USER_ID, STATE_DATA, VERSION)
+        model = QuestionnaireState(USER_ID, STATE_DATA, VERSION, COLLECTION_EXERCISE_ID, FORM_TYPE, RU_REF, EQ_ID)
 
         with mock.patch('app.storage.dynamo_api.put_item') as put_item:
             data_access.put(model)
@@ -65,7 +70,7 @@ class TestDataAccess(AppContextTestCase):
         self.assertTrue(put_item.call_args[1]['overwrite'])
 
     def test_put_rds(self):
-        model = QuestionnaireState(USER_ID, STATE_DATA, VERSION)
+        model = QuestionnaireState(USER_ID, STATE_DATA, VERSION, COLLECTION_EXERCISE_ID, FORM_TYPE, RU_REF, EQ_ID)
         setattr(model, '_use_dynamo', False)
 
         with mock.patch('app.data_model.models.db.session.merge') as merge:
@@ -79,7 +84,7 @@ class TestDataAccess(AppContextTestCase):
         self.assertEqual(rds_model.version, VERSION)
 
     def test_put_no_overwrite(self):
-        model = QuestionnaireState(USER_ID, STATE_DATA, VERSION)
+        model = QuestionnaireState(USER_ID, STATE_DATA, VERSION, COLLECTION_EXERCISE_ID, FORM_TYPE, RU_REF, EQ_ID)
 
         side_effect = ClientError({'Error': {'Code': 'ConditionalCheckFailedException'}}, 'PutItem')
         with mock.patch('app.storage.dynamo_api.put_item', side_effect=side_effect) as put_item, \
@@ -89,7 +94,7 @@ class TestDataAccess(AppContextTestCase):
         self.assertFalse(put_item.call_args[1]['overwrite'])
 
     def test_put_rds_no_overwrite(self):
-        model = QuestionnaireState(USER_ID, STATE_DATA, VERSION)
+        model = QuestionnaireState(USER_ID, STATE_DATA, VERSION, COLLECTION_EXERCISE_ID, FORM_TYPE, RU_REF, EQ_ID)
         setattr(model, '_use_dynamo', False)
 
         with mock.patch('app.data_model.models.db.session.add', side_effect=IntegrityError('', '', '')) as add, \
@@ -99,7 +104,7 @@ class TestDataAccess(AppContextTestCase):
         self.assertTrue(add.called)
 
     def test_put_propogate_dynamo_exception(self):
-        model = QuestionnaireState(USER_ID, STATE_DATA, VERSION)
+        model = QuestionnaireState(USER_ID, STATE_DATA, VERSION, COLLECTION_EXERCISE_ID, FORM_TYPE, RU_REF, EQ_ID)
 
         side_effect = ClientError({'Error': {'Code': 'RandomError'}}, 'PutItem')
         with mock.patch('app.storage.dynamo_api.put_item', side_effect=side_effect) as put_item, \
@@ -109,7 +114,7 @@ class TestDataAccess(AppContextTestCase):
         self.assertFalse(put_item.call_args[1]['overwrite'])
 
     def test_delete(self):
-        model = QuestionnaireState(USER_ID, STATE_DATA, VERSION)
+        model = QuestionnaireState(USER_ID, STATE_DATA, VERSION, COLLECTION_EXERCISE_ID, FORM_TYPE, RU_REF, EQ_ID)
 
         with mock.patch('app.storage.dynamo_api.delete_item') as delete_item:
             data_access.delete(model)
@@ -117,7 +122,7 @@ class TestDataAccess(AppContextTestCase):
         self.assertEqual(delete_item.call_args[0][1], {'user_id': USER_ID})
 
     def test_delete_rds(self):
-        model = QuestionnaireState(USER_ID, STATE_DATA, VERSION)
+        model = QuestionnaireState(USER_ID, STATE_DATA, VERSION, COLLECTION_EXERCISE_ID, FORM_TYPE, RU_REF, EQ_ID)
         setattr(model, '_use_dynamo', False)
 
         with mock.patch('app.data_model.models.db.session.delete') as delete:
@@ -129,7 +134,7 @@ class TestDataAccess(AppContextTestCase):
         self.assertEqual(rds_model.user_id, USER_ID)
 
     def test_dymo_no_write_when_read_disabled(self): # pylint: disable=no-self-use
-        model = QuestionnaireState(USER_ID, STATE_DATA, VERSION)
+        model = QuestionnaireState(USER_ID, STATE_DATA, VERSION, COLLECTION_EXERCISE_ID, FORM_TYPE, RU_REF, EQ_ID)
         with mock.patch('app.storage.data_access.is_dynamodb_read_enabled', mock.Mock(return_value=False)), \
                 mock.patch('app.storage.data_access.models.db.session.add') as merge:
             data_access.put(model, overwrite=False)
