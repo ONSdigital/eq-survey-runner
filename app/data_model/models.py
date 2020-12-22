c7ee2931915ef8d16f7334d2326a1af2ae82804c
Matthew Elcock <mattiose@outlook.com>
2020-12-22 09:54:34 +0000

    Updated legacy db

--- app/data_model/models.py
+++ app/data_model/models.py
@@ -15,16 +15,24 @@ class QuestionnaireState(db.Model):
     user_id = db.Column('userid', db.String, primary_key=True)
     state = db.Column('questionnaire_data', db.String)
     version = db.Column('version', db.Integer)
+    collection_exercise_id = db.Column('collection_exercise_id', db.String)
+    form_type = db.Column('form_type', db.String)
+    ru_ref = db.Column('ru_ref', db.String)
+    eq_id = db.Column('eq_id', db.String)
     created_at = db.Column('created_at', db.DateTime, default=datetime.datetime.utcnow)
     updated_at = db.Column('updated_at', db.DateTime, default=datetime.datetime.utcnow, onupdate=datetime.datetime.utcnow)
 
-    def __init__(self, user_id, state, version):
+    def __init__(self, user_id, state, version, collection_exercise_id, form_type, ru_ref, eq_id):
         self.user_id = user_id
         self.state = state
         self.version = version
+        self.collection_exercise_id = collection_exercise_id
+        self.form_type = form_type
+        self.ru_ref = ru_ref
+        self.eq_id = eq_id
 
     def to_app_model(self):
-        model = app_models.QuestionnaireState(self.user_id, self.state, self.version)
+        model = app_models.QuestionnaireState(self.user_id, self.state, self.version, self.collection_exercise_id, self.form_type, self.ru_ref, self.eq_id)
         model.created_at = self.created_at
         model.updated_at = self.updated_at
         return model
