1932cfd922c8e882ecba770413ced2354b6bb8e2
Matthew Elcock <mattiose@outlook.com>
2020-12-22 12:59:18 +0000

    Updated legacy db

--- tests/app/storage/test_encrypted_questionnaire_storage.py
+++ tests/app/storage/test_encrypted_questionnaire_storage.py
@@ -182,10 +182,11 @@ class TestEncryptedQuestionnaireStorageEncoding(AppContextTestCase):
         self._save_compressed_state_data(self.user_id, json_data)
         
         user_data_from_storage = self.storage.get_user_data()
-        json_data_from_user = user_data_from_storage[0]
+        data_from_user = json.loads(user_data_from_storage[0])
         data_version_from_user = user_data_from_storage[1]
         
-        self.assertEqual((json_data, QuestionnaireStore.LATEST_VERSION + 1), (json_data_from_user, data_version_from_user))
+        self.assertEqual(data_from_user, mock_data)
+        self.assertEqual(QuestionnaireStore.LATEST_VERSION + 1, data_version_from_user)
 
     def _save_legacy_state_data(self, user_id, data):
         json_data = json.loads(data)
